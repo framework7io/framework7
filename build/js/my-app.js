0a1b12ab19f75971eade83c4e156c829368e0007
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-02-23 20:49:18 +0400

    Remove build/

--- build/js/my-app.js
+++
@@ -1,8 +0,0 @@
-// Initialize your app
-var myApp = new Framework7();
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
