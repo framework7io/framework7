a6dcb78e5711b3ad6aac660babe2506f2bf4bd9e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-04-09 13:21:20 +0400

    Moved fast clicks to separate file

--- src/js/clicks.js
+++ src/js/clicks.js
@@ -1,71 +1,8 @@
 /*===============================================================================
-************   Fast Clicks   ************
-===============================================================================*/
-app.initFastClicks = function () {
-    if (!$.supportTouch) return;
-    var touchStartX, touchStartY, touchStartTime, targetElement, trackClick, activeSelection;
-
-    function targetNeedsFocus(el) {
-        var tag = el.nodeName.toLowerCase();
-        var tags = ['textarea', 'select'];
-        var skipInputs = ('button checkbox file image radio submit').split(' ');
-        if (el.disabled || el.readOnly) return false;
-        if (tag === 'textarea' || tag === 'select') return true;
-        if (tag === 'input' && skipInputs.indexOf(el.type) < 0) return true;
-    }
-    function handleTouchStart(e) {
-        if (e.targetTouches.length > 1) {
-            return true;
-        }
-        if (app.device.os === 'ios') {
-            var selection = window.getSelection();
-            if (selection.rangeCount && !selection.isCollapsed) {
-                activeSelection = true;
-                return true;
-            }
-        }
-        trackClick = true;
-        targetElement = e.target;
-        touchStartTime = (new Date()).getTime();
-        touchStartX = e.targetTouches[0].pageX;
-        touchStartY = e.targetTouches[0].pageY;
-        
-    }
-    function handleTouchMove(e) {
-        if (!trackClick) return;
-        trackClick = false;
-        targetElement = null;
-    }
-    function handleTouchEnd(e) {
-        if (!trackClick) {
-            if (!activeSelection) e.preventDefault();
-            return true;
-        }
-        e.preventDefault();
-
-        var touchEndTime = (new Date()).getTime();
-        trackClick = false;
-
-        // Trigger focus where required
-        if (targetNeedsFocus(targetElement)) targetElement.focus();
-
-        // Trigger click
-        $(targetElement).trigger('click');
-    }
-    function handleClick(e) {
-        
-    }
-    $(document).on('touchstart', handleTouchStart);
-    $(document).on('touchmove', handleTouchMove);
-    $(document).on('touchend', handleTouchEnd);
-    $(document).on('click', handleClick);
-    
-};
-/*===============================================================================
 ************   Handle clicks and make them fast (on tap);   ************
 ===============================================================================*/
 app.initClickEvents = function () {
-    function handleTap(e) {
+    function handleClicks(e) {
         /*jshint validthis:true */
         var clicked = $(this);
         var url = clicked.attr('href');
@@ -127,13 +64,6 @@ app.initClickEvents = function () {
             if ($('.popover.modal-in').length > 0) app.closeModal('.popover.modal-in');
         }
         
-        if ($.supportTouch) {
-            if (clicked.parent().hasClass('label-switch')) {
-                clicked[0].checked = !clicked[0].checked;
-                clicked.trigger('change');
-            }
-        }
-        
         // Tabs
         if (clicked.hasClass('tab-link')) {
             var newTab = $(clicked.attr('href'));
@@ -183,5 +113,5 @@ app.initClickEvents = function () {
             else view.loadPage(clicked.attr('href'));
         }
     }
-    $(document).on('click', 'a, .open-panel, .close-panel, .panel-overlay, .modal-overlay, .swipeout-delete, .close-popup, .open-popup, .open-popover, .label-switch, .label-switch input', handleTap);
+    $(document).on('click', 'a, .open-panel, .close-panel, .panel-overlay, .modal-overlay, .swipeout-delete, .close-popup, .open-popup, .open-popover', handleClicks);
 };
