63417b693bebca36644ce9c5628516f8ef8c0d9d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-04-10 20:50:11 +0400

    Selectors engine

--- examples/tab-bar/js/my-app.js
+++ examples/tab-bar/js/my-app.js
@@ -1,7 +1,10 @@
 // Initialize your app
 var myApp = new Framework7();
 
-// Add view
+// Export selectors engine
+var $$ = Framework7.$;
+
+// Add views
 var view1 = myApp.addView('#view-1');
 var view2 = myApp.addView('#view-2', {
     // Because we use fixed-through navbar we can enable dynamic navbar
