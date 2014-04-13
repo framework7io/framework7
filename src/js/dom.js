9cab768d8a740482c0fea616fcce9271a6365afd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-04-13 19:41:48 +0400

    Updated .transition method

--- src/js/dom.js
+++ src/js/dom.js
@@ -76,9 +76,12 @@ Dom7.prototype = {
         return this;
     },
     transition: function (duration) {
+        if (typeof duration !== 'string') {
+            duration = duration + 'ms';
+        }
         for (var i = 0; i < this.length; i++) {
             var elStyle = this[i].style;
-            elStyle.webkitTransitionDuration = elStyle.MsTransitionDuration = elStyle.msTransitionDuration = elStyle.MozTransitionDuration = elStyle.OTransitionDuration = elStyle.transitionDuration = duration + 'ms';
+            elStyle.webkitTransitionDuration = elStyle.MsTransitionDuration = elStyle.msTransitionDuration = elStyle.MozTransitionDuration = elStyle.OTransitionDuration = elStyle.transitionDuration = duration;
         }
         return this;
     },
