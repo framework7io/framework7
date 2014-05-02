c1a4b59040d0982225fbbdd36f4d547060277669
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-05-02 13:19:30 +0400

    .css() method with (prop, value) arguments

--- src/js/dom.js
+++ src/js/dom.js
@@ -269,19 +269,28 @@ Dom7.prototype = {
         }
         return this;
     },
-    css: function (props) {
-        if (typeof props === 'string') {
-            if (this[0]) return window.getComputedStyle(this[0], null).getPropertyValue(props);
-        }
-        else {
-            for (var i = 0; i < this.length; i++) {
-                for (var prop in props) {
-                    this[i].style[prop] = props[prop];
+    css: function (props, value) {
+        var i;
+        if (arguments.length === 1) {
+            if (typeof props === 'string') {
+                if (this[0]) return window.getComputedStyle(this[0], null).getPropertyValue(props);
+            }
+            else {
+                for (i = 0; i < this.length; i++) {
+                    for (var prop in props) {
+                        this[i].style[prop] = props[prop];
+                    }
                 }
+                return this;
+            }
+        }
+        if (arguments.length === 2 && typeof props === 'string') {
+            for (i = 0; i < this.length; i++) {
+                this[i].style[props] = value;
             }
             return this;
         }
-        
+        return this;
     },
     
     //Dom manipulation
