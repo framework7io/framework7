6b812348c2cda5a3312db542f77f0f9bc14344a0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-05-02 12:56:02 +0400

    Remove sizeNavbars on panel open

--- examples/split-view-panel/js/my-app.js
+++ examples/split-view-panel/js/my-app.js
@@ -13,8 +13,3 @@ var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
-
-$$('.panel-left').on('open', function () {
-    // We need to size navbar to set correct position on navbar elements when we open left panel
-    myApp.sizeNavbars($$('.view-left'));
-});
