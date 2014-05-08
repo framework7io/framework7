36db3dbb59769e91e9ff1fa2eb55139f8d65f1c1
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-05-08 21:23:00 +0400

    New cacheIgnoreList parameter

--- src/js/f7-intro.js
+++ src/js/f7-intro.js
@@ -20,7 +20,7 @@ window.Framework7 = function (params) {
     // Default Parameters
     app.params = {
         cache: true,
-        cacheSkip: [],
+        cacheIgnoreList: [],
         cacheDuration: 1000 * 60 * 10, // Ten minutes 
         preloadPreviousPage: true,
         // Push State
