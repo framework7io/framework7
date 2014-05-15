1ff05573bcfb5439aafb09f01e16774bb2d922ac
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-05-15 19:27:39 +0400

    Improved .index() method

--- src/js/dom.js
+++ src/js/dom.js
@@ -343,8 +343,9 @@ Dom7.prototype = {
         if (this[0]) {
             var child = this[0];
             var i = 0;
-            while ((child = child.previousSibling) != null)
-                i++;
+            while ((child = child.previousSibling) != null) {
+                if (child.nodeType === 1) i++;
+            }
             return i;
         }
         else return undefined;
