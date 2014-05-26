77f00cfa07361bbb8bee5bcb424c6b972336d071
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-05-26 13:15:50 +0400

    Tabs handler

---
+++ src/js/tabs.js
@@ -0,0 +1,40 @@
+/* ===============================================================================
+************   Tabs   ************
+=============================================================================== */
+app.showTab = function (tab, tabLink) {
+    var newTab = $(tab);
+    if (newTab.length === 0) return false;
+    var tabs = newTab.parent('.tabs');
+    if (tabs.length === 0) return false;
+
+    // Animated tabs
+    var isAnimatedTabs = tabs.parent().hasClass('tabs-animated-wrap');
+    if (isAnimatedTabs) {
+        tabs.transform('translate3d(' + -newTab.index() * 100 + '%,0,0)');
+    }
+
+    // Remove active class from old tabs
+    tabs.children('.tab.active').removeClass('active');
+    // Add active class to new tab
+    newTab.addClass('active');
+
+    // Update navbars in new tab
+    if (!isAnimatedTabs && newTab.find('.navbar').length > 0) {
+        // Find tab's view
+        var viewContainer;
+        if (newTab.hasClass(app.params.viewClass)) viewContainer = newTab[0];
+        else viewContainer = newTab.parents('.' + app.params.viewClass)[0];
+        app.sizeNavbars(viewContainer);
+    }
+
+    // Update class on tab-links
+    if (tabLink) tabLink = $(tabLink);
+    else {
+        if (typeof tab === 'string') tabLink = $('.tab-link[href="#' + tab + '"]');
+        else tabLink = $('.tab-link[href="#' + newTab.attr('id') + '"]');
+    }
+    if (tabLink.length === 0) return;
+
+    tabLink.parent().find('.active').removeClass('active');
+    tabLink.addClass('active');
+};
