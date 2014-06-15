05ffc96402ab9a5cd82b4dd4c08bf0326052297f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-06-15 16:21:25 +0400

    Notification demos

--- kitchen-sink/js/kitchen-sink.js
+++ kitchen-sink/js/kitchen-sink.js
@@ -268,6 +268,42 @@ $$(document).on('pageInit', function (e) {
             });
         });
     }
+    // Notifications
+    if (page.name === 'notifications') {
+        $$('.ks-notification-simple').on('click', function () {
+            myApp.addNotification({
+                title: 'Framework7',
+                message: 'This is a simple notification message with title and message'
+            });
+        });
+        $$('.ks-notification-full').on('click', function () {
+            myApp.addNotification({
+                title: 'Framework7',
+                subtitle: 'Notification subtitle',
+                message: 'This is a simple notification message with custom icon and subtitle',
+                media: '<i class="icon icon-f7"></i>'
+            });
+        });
+        $$('.ks-notification-custom').on('click', function () {
+            myApp.addNotification({
+                title: 'My Awesome App',
+                subtitle: 'New message from John Doe',
+                message: 'Hello, how are you? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut posuere erat. Pellentesque id elementum urna, a aliquam ante. Donec vitae volutpat orci. Aliquam sed molestie risus, quis tincidunt dui.',
+                media: '<img width="44" height="44" style="border-radius:100%" src="http://lorempixel.com/output/people-q-c-100-100-9.jpg">'
+            });
+        });
+        $$('.ks-notification-callback').on('click', function () {
+            myApp.addNotification({
+                title: 'My Awesome App',
+                subtitle: 'New message from John Doe',
+                message: 'Hello, how are you? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut posuere erat. Pellentesque id elementum urna, a aliquam ante. Donec vitae volutpat orci. Aliquam sed molestie risus, quis tincidunt dui.',
+                media: '<img width="44" height="44" style="border-radius:100%" src="http://lorempixel.com/output/people-q-c-100-100-9.jpg">',
+                onClose: function () {
+                    myApp.alert('Notification closed');
+                }
+            });
+        });
+    }
 });
 
 // Required for demo popover
