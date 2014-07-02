bdb265025be81a7f85fd570775bded7d36abc314
Greg Keys <gregkeys+github@gmail.com>
2014-07-01 17:18:47 -0700

    Added Manual Hoisting to all var's and brackets to single line conditional statements

--- src/js/clicks.js
+++ src/js/clicks.js
@@ -1,12 +1,15 @@
 /*===============================================================================
-************   Handle clicks and make them fast (on tap);   ************
-===============================================================================*/
+ ************   Handle clicks and make them fast (on tap);   ************
+ ===============================================================================*/
 app.initClickEvents = function () {
     function handleClicks(e) {
         /*jshint validthis:true */
-        var clicked = $(this);
-        var url = clicked.attr('href');
-        var isLink = clicked[0].nodeName.toLowerCase() === 'a';
+        var clicked = $(this),
+            url = clicked.attr('href'),
+            isLink = clicked[0].nodeName.toLowerCase() === 'a',
+            panel, popover, popup,
+            text, title,
+            validUrl, view, i, animatePages;
         // External
         if (isLink && clicked.hasClass('external')) {
             return;
@@ -15,16 +18,25 @@ app.initClickEvents = function () {
         if (clicked.hasClass('smart-select')) {
             if (app.smartSelectOpen) app.smartSelectOpen(clicked);
         }
-        
+
         // Open Panel
         if (clicked.hasClass('open-panel')) {
-            if ($('.panel').length === 1) {
-                if ($('.panel').hasClass('panel-left')) app.openPanel('left');
-                else app.openPanel('right');
+            panel = $('.panel');
+            if (panel.length === 1) {
+                if (panel.hasClass('panel-left')) {
+                    app.openPanel('left');
+                }
+                else {
+                    app.openPanel('right');
+                }
             }
             else {
-                if (clicked.attr('data-panel') === 'right') app.openPanel('right');
-                else app.openPanel('left');
+                if (clicked.attr('data-panel') === 'right') {
+                    app.openPanel('right');
+                }
+                else {
+                    app.openPanel('left');
+                }
             }
         }
         // Close Panel
@@ -37,20 +49,22 @@ app.initClickEvents = function () {
         }
         // Popover
         if (clicked.hasClass('open-popover')) {
-            var popover;
             if (clicked.attr('data-popover')) {
                 popover = clicked.attr('data-popover');
             }
-            else popover = '.popover';
+            else {
+                popover = '.popover';
+            }
             app.popover(popover, clicked);
         }
         // Popup
-        var popup;
         if (clicked.hasClass('open-popup')) {
             if (clicked.attr('data-popup')) {
                 popup = clicked.attr('data-popup');
             }
-            else popup = '.popup';
+            else {
+                popup = '.popup';
+            }
             app.popup(popup);
         }
         if (clicked.hasClass('close-popup')) {
@@ -58,16 +72,20 @@ app.initClickEvents = function () {
         }
         // Close Modal
         if (clicked.hasClass('modal-overlay')) {
-            if ($('.modal.modal-in').length > 0 && app.params.modalCloseByOutside)
+            if ($('.modal.modal-in').length > 0 && app.params.modalCloseByOutside) {
                 app.closeModal();
-            if ($('.actions-modal.modal-in').length > 0 && app.params.actionsCloseByOutside)
+            }
+            if ($('.actions-modal.modal-in').length > 0 && app.params.actionsCloseByOutside) {
                 app.closeModal();
-            
-            if ($('.popover.modal-in').length > 0) app.closeModal('.popover.modal-in');
+            }
+            if ($('.popover.modal-in').length > 0) {
+                app.closeModal('.popover.modal-in');
+            }
         }
         if (clicked.hasClass('popup-overlay')) {
-            if ($('.popup.modal-in').length > 0 && app.params.popupCloseByOutside)
+            if ($('.popup.modal-in').length > 0 && app.params.popupCloseByOutside) {
                 app.closeModal('.popup.modal-in');
+            }
         }
 
         // Tabs
@@ -77,8 +95,8 @@ app.initClickEvents = function () {
         // Swipeout Delete
         if (clicked.hasClass('swipeout-delete')) {
             if (clicked.attr('data-confirm')) {
-                var text = clicked.attr('data-confirm');
-                var title = clicked.attr('data-confirm-title');
+                text = clicked.attr('data-confirm');
+                title = clicked.attr('data-confirm-title');
                 if (title) {
                     app.confirm(text, title, function () {
                         app.swipeoutDelete(clicked.parents('.swipeout'));
@@ -93,7 +111,7 @@ app.initClickEvents = function () {
             else {
                 app.swipeoutDelete(clicked.parents('.swipeout'));
             }
-                
+
         }
         // Sortable
         if (clicked.hasClass('toggle-sortable')) {
@@ -112,9 +130,9 @@ app.initClickEvents = function () {
         if (isLink) {
             e.preventDefault();
         }
-        var validUrl = url && url.length > 0 && url.indexOf('#') !== 0;
+
+        validUrl = url && url.length > 0 && url.indexOf('#') !== 0;
         if (validUrl || clicked.hasClass('back')) {
-            var view;
             if (clicked.attr('data-view')) {
                 view = $(clicked.attr('data-view'))[0].f7View;
             }
@@ -125,16 +143,24 @@ app.initClickEvents = function () {
                 }
             }
             if (!view) {
-                for (var i = 0; i < app.views.length; i++) {
+                for (i = 0; i < app.views.length; i++) {
                     if (app.views[i].main) view = app.views[i];
                 }
             }
-            if (!view) return;
-            var animatePages;
-            if (clicked.hasClass('no-animation')) animatePages = false;
-            if (clicked.hasClass('back')) view.goBack(clicked.attr('href'), animatePages);
-            else view.loadPage(clicked.attr('href'), animatePages);
+            if (!view) {
+                return;
+            }
+            if (clicked.hasClass('no-animation')) {
+                animatePages = false;
+            }
+            if (clicked.hasClass('back')) {
+                view.goBack(clicked.attr('href'), animatePages);
+            }
+            else {
+                view.loadPage(clicked.attr('href'), animatePages);
+            }
         }
     }
+
     $(document).on('click', 'a, .open-panel, .close-panel, .panel-overlay, .modal-overlay, .popup-overlay, .swipeout-delete, .close-popup, .open-popup, .open-popover, .smart-select, .toggle-sortable, .open-sortable, .close-sortable', handleClicks);
 };
