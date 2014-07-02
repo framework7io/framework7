063df732e3051795446e165850f4f9a1ecc4341f
Greg Keys <gregkeys+github@gmail.com>
2014-07-01 21:33:13 -0700

    Added Manual Hoisting to all var's and brackets to single line conditional statements

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,15 +1,21 @@
 /* ===============================================================================
-************   Tabs   ************
-=============================================================================== */
+ ************   Tabs   ************
+ =============================================================================== */
 app.showTab = function (tab, tabLink) {
-    var newTab = $(tab);
-    if (newTab.hasClass('active')) return false;
-    if (newTab.length === 0) return false;
-    var tabs = newTab.parent('.tabs');
-    if (tabs.length === 0) return false;
+    var newTab = $(tab), tabs, isAnimatedTabs, viewContainer;
+    if (newTab.hasClass('active')) {
+        return false;
+    }
+    if (newTab.length === 0) {
+        return false;
+    }
+    tabs = newTab.parent('.tabs');
+    if (tabs.length === 0) {
+        return false;
+    }
 
     // Animated tabs
-    var isAnimatedTabs = tabs.parent().hasClass('tabs-animated-wrap');
+    isAnimatedTabs = tabs.parent().hasClass('tabs-animated-wrap');
     if (isAnimatedTabs) {
         tabs.transform('translate3d(' + -newTab.index() * 100 + '%,0,0)');
     }
@@ -24,22 +30,33 @@ app.showTab = function (tab, tabLink) {
     // Update navbars in new tab
     if (!isAnimatedTabs && newTab.find('.navbar').length > 0) {
         // Find tab's view
-        var viewContainer;
-        if (newTab.hasClass(app.params.viewClass)) viewContainer = newTab[0];
-        else viewContainer = newTab.parents('.' + app.params.viewClass)[0];
+        if (newTab.hasClass(app.params.viewClass)) {
+            viewContainer = newTab[0];
+        }
+        else {
+            viewContainer = newTab.parents('.' + app.params.viewClass)[0];
+        }
         app.sizeNavbars(viewContainer);
     }
 
     // Update class on tab-links
-    if (tabLink) tabLink = $(tabLink);
-    else {
-        if (typeof tab === 'string') tabLink = $('.tab-link[href="' + tab + '"]');
-        else tabLink = $('.tab-link[href="#' + newTab.attr('id') + '"]');
+    if (tabLink) {
+        tabLink = $(tabLink);
     }
-    if (tabLink.length === 0) return;
+    else {
+        if (typeof tab === 'string') {
+            tabLink = $('.tab-link[href="' + tab + '"]');
+        }
+        else {
+            tabLink = $('.tab-link[href="#' + newTab.attr('id') + '"]');
+        }
+    }
+    if (tabLink.length === 0) {
+        return;
+    }
 
     tabLink.parent().find('.active').removeClass('active');
     tabLink.addClass('active');
-    
+
     return true;
 };
