96c6dc3d81b069ed92c7aad56d27cd37d21f8836
Thomas <thomas@furch.com>
2014-08-05 13:26:05 +0200

    removed export (was in there for debugging)

--- examples/inline-templates/js/my-app.js
+++ examples/inline-templates/js/my-app.js
@@ -4,7 +4,7 @@ var myApp = new Framework7({preloadPreviousPage: false, pushState: true});
 // Export selectors engine
 var $$ = Framework7.$;
 
-window.mainView = myApp.addView('.view-main', {
+var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true,
     domCache: true
