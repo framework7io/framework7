336334d553ecd74192cb7007c2b46094dfff2fa7
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-08-27 01:22:35 +0400

    Fixed transitionEnd target

--- src/js/dom7-methods.js
+++ src/js/dom7-methods.js
@@ -193,6 +193,7 @@ Dom7.prototype = {
             i, j, dom = this;
         function fireCallBack(e) {
             /*jshint validthis:true */
+            if (e.target !== this) return;
             callback.call(this, e);
             for (i = 0; i < events.length; i++) {
                 dom.off(events[i], fireCallBack);
