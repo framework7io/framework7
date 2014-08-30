eafbe771aeb3a4fd69efd4a963ca4a583ea884ec
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-08-30 19:01:55 +0400

    Template7 Update

--- src/js/template7.js
+++ src/js/template7.js
@@ -43,8 +43,8 @@ window.Template7 = (function () {
                 var helperName = helperSlices[0];
                 var helperContext = helperSlices[1];
                 var helperHash = {};
-
-                if (helperSlices.length > 2) {
+                
+                if (helperSlices.length > 1) {
                     var hashRes;
                     var reg = new RegExp(/\b(\w+)=["']([^"']+)(?=["'])/g);
                     while ((hashRes = reg.exec(block)) !== null) {
@@ -122,176 +122,92 @@ window.Template7 = (function () {
         t.template = template;
         t.context = data;
         
-        function getContext(contextName, context, privateData) {
-            if (contextName === 'this' || typeof contextName === 'undefined') {
-                return context;
-            }
-
-            if (contextName.indexOf('@') >= 0) {
-                //private data
-                var privateVarName = contextName.replace(/[@ ]/g, '');
-                if (privateData && typeof privateData[privateVarName] !== 'undefined') return privateData[privateVarName];
-            }
+        function getCompileFn(block, depth) {
+            if (block.content) return compile(block.content, depth);
+            else return function () {return ''; };
+        }
+        function getCompileInverse(block, depth) {
+            if (block.inverseContent) return compile(block.inverseContent, depth);
+            else return function () {return ''; };
+        }
+        function getCompileVar(name, ctx) {
+            var parents, variable, context;
             
-            if (contextName.indexOf('.') > 0 && contextName.indexOf('../') < 0) {
-                var dataPath = contextName.split('.');
-                var _context = context;
-                for (var j = 0; j < dataPath.length; j++) {
-                    if (dataPath[j] === 'this') {
-                        _context = context;
-                    }
-                    else if (typeof _context !== 'undefined' && isObject(context) && dataPath[j] in _context) {
-                        _context = _context[dataPath[j]];
-                    }
-                    else {
-                        _context = undefined;
-                    }
-                }
-                context = _context;
+            if (name.indexOf('.') > 0) {
+                if (name.indexOf('this') === 0) variable = name.replace('this', ctx);
+                else variable = ctx + '.' + name;
             }
-            else if (contextName.indexOf('../') >= 0) {
-            }
-            else if (context[contextName]) {
-                context = context[contextName];
+            else if (name.indexOf('../') === 0) {
+                var levelUp = name.split('../').length - 1;
+                var newName = name.split('../')[name.split('../').length - 1];
+                var newDepth = ctx.split('_')[1] - levelUp;
+                variable = 'ctx_' + (newDepth >= 1 ? newDepth : 1) + '.' + newName;
             }
             else {
-                context = undefined;
+                variable = name === 'this' ? ctx : ctx + '.' + name;
             }
-            return context;
-        }
-        function createOptions(block, privateData) {
-            return {
-                fn: function (newContext, privateData) {
-                    return render(block.content, newContext, privateData);
-                },
-                inverse: function (newContext, privateData) {
-                    return block.inverseContent ? render(block.inverseContent, newContext, privateData) : '';
-                },
-                hash: block.hash || {},
-                helpers: t.helpers,
-                data: privateData
-            };
-        }
-        function render(template, data, privateData) {
-            template = template || t.template;
-            if (typeof template !== 'string') {
-                throw new Error('Template7: Template must be a string');
-            }
-            var scope = data || t.context;
-            var blocks = stringToBlocks(template);
-            var resultString = '';
-            var i, j, context;
-            for (i = 0; i < blocks.length; i++) {
-                var block = blocks[i];
-                // Plain block
-                if (block.type === 'plain') {
-                    resultString += block.content;
-                    continue;
-                }
-                // Variable block
-                if (block.type === 'variable') {
-                    context = getContext(block.contextName, scope, privateData);
-                    if (typeof context !== 'undefined') {
-                        resultString += context.toString();
-                    }
-                }
-                // Helpers block
-                if (block.type === 'helper') {
-                    context = getContext(block.contextName, scope, privateData);
-                    // Create options
-                    var options = createOptions(block, privateData);
-                    var helperResult;
-                    var helperName = block.helperName;
-                    if (block.helperName in t.helpers) {
-                        helperResult = t.helpers[helperName].call(scope, context, options);
-                    }
-                    else {
-                        if (!block.contextName && (helperName === 'this' || (isObject(context) && helperName in context))) {
-                            var _context = helperName === 'this' ? context : context[helperName];
-                            if (isArray(_context)) helperResult = t.helpers.each.call(scope, context[helperName], options);
-                            else helperResult = render(block.content, context[helperName]);
-                        }
-                        else {
-                            throw new Error('Template7: Missing helper: "' + helperName + '"');
-                        }
-                    }
-                    if (typeof helperResult !== 'undefined') {
-                        resultString += helperResult.toString();
-                    }
-                }
-            }
-            return resultString;
-        }
-
-        var depth = 0;
-        function getCompileFn(block) {
-            if (block.content) return compile(block.content);
-            else return function () {return ''; };
-        }
-        function getCompileInverse(block) {
-            if (block.inverseContent) return compile(block.inverseContent);
-            else return function () {return ''; };
-        }
-        function getCompileVar(name) {
-            var variable = name === 'this' ? 'ctx' : 'ctx.' + name;
             if (name && name.indexOf('@') >= 0) {
                 variable = '(data && data.' + name.replace('@', '') + ')';
             }
+                
             return variable;
         }
-        function compile(template) {
+        function compile(template, depth) {
+            depth = depth || 1;
             template = template || t.template;
             if (typeof template !== 'string') {
                 throw new Error('Template7: Template must be a string');
             }
             var blocks = stringToBlocks(template);
+            
             if (blocks.length === 0) {
                 return function () { return ''; };
             }
-            var resultString = '(function (ctx, data) {\n';
-            if (depth === 0) {
-                resultString += 'function c(val) {if (typeof val !== "undefined") return val; else return ""}\n';
+            var ctx = 'ctx_' + depth;
+            var resultString = '(function (' + ctx + ', data) {\n';
+            if (depth === 1) {
                 resultString += 'function isArray(arr){return Object.prototype.toString.apply(arr) === \'[object Array]\';}\n';
+                resultString += 'function isFunction(func){return (typeof func === \'function\');}\n';
+                resultString += 'function c(val, ctx) {if (typeof val !== "undefined") {if (isFunction(val)) {return val.call(ctx);} else return val;} else return "";}\n';
             }
-            depth ++;
-            resultString += 'var ret = \'\';\n';
+            resultString += 'var r = \'\';\n';
             var i, j, context;
             for (i = 0; i < blocks.length; i++) {
                 var block = blocks[i];
                 // Plain block
                 if (block.type === 'plain') {
-                    resultString += 'ret +=\'' + (block.content).replace(/\n/g, '\\n').replace(/'/g, '\\' + '\'') + '\';';
+                    resultString += 'r +=\'' + (block.content).replace(/\n/g, '\\n').replace(/'/g, '\\' + '\'') + '\';';
                     continue;
                 }
                 var variable;
                 // Variable block
                 if (block.type === 'variable') {
-                    variable = getCompileVar(block.contextName);
-                    resultString += 'ret += c(' + variable + ');';
+                    variable = getCompileVar(block.contextName, ctx);
+                    resultString += 'r += c(' + variable + ', ' + ctx + ');';
                 }
                 // Helpers block
                 if (block.type === 'helper') {
                     if (block.helperName in t.helpers) {
-                        variable = getCompileVar(block.contextName);
-                        resultString += 'ret += (Template7.helpers.' + block.helperName + ').call(ctx, ' + variable + ', {hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block) + ', inverse: ' + getCompileInverse(block) + '});';
+                        variable = getCompileVar(block.contextName, ctx);
+                        resultString += 'r += (Template7.helpers.' + block.helperName + ').call(' + ctx + ', ' + variable + ', {hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block, depth+1) + ', inverse: ' + getCompileInverse(block, depth+1) + '});';
                     }
                     else {
                         if (block.contextName) {
                             throw new Error('Template7: Missing helper: "' + block.helperName + '"');
                         }
                         else {
-                            variable = getCompileVar(block.helperName);
+                            variable = getCompileVar(block.helperName, ctx);
                             resultString += 'if (' + variable + ') {';
                             resultString += 'if (isArray(' + variable + ')) {';
-                            resultString += 'ret += (Template7.helpers.each).call(ctx, ' + variable + ', {hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block) + ', inverse: ' + getCompileInverse(block) + '});';
+                            resultString += 'r += (Template7.helpers.each).call(' + ctx + ', ' + variable + ', {hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block, depth+1) + ', inverse: ' + getCompileInverse(block, depth+1) + '});';
                             resultString += '}else {';
-                            resultString += 'ret += (Template7.helpers.with).call(ctx, ' + variable + ', {hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block) + ', inverse: ' + getCompileInverse(block) + '});';
+                            resultString += 'r += (Template7.helpers.with).call(' + ctx + ', ' + variable + ', {hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block, depth+1) + ', inverse: ' + getCompileInverse(block, depth+1) + '});';
                             resultString += '}}';
                         }
                     }
                 }
             }
-            resultString += '\nreturn ret;})';
+            resultString += '\nreturn r;})';
             return eval.call(window, resultString);
         }
         t.compile = function (template) {
@@ -300,32 +216,10 @@ window.Template7 = (function () {
             }
             return t.compiled;
         };
-        t.render = function (data) {
-            t.context = data;
-            t.prevContext = undefined;
-            var rendered, cached, id;
-            if (t.options.cache) {
-                for (var key in cache) {
-                    if (key.indexOf('template_') >= 0 && cache[key] === t.template) {
-                        id = key.split('template_')[1];
-                        if (cache['data_' + id] === JSON.stringify(data)) cached = cache['rendered_' + id];
-                    }
-                }
-                if (cached) return cached;
-            }
-            rendered = render(t.template, data);
-            if (t.options.cache) {
-                id = new Date().getTime();
-                cache['template_' + id] = t.template;
-                cache['data_' + id] = JSON.stringify(data);
-                cache['rendered_' + id] = rendered;
-            }
-            return rendered;
-        };
     };
     Template7.prototype = {
         options: {
-            cache: false
+            // cache: false
         },
         helpers: {
             'if': function (context, options) {
@@ -384,7 +278,7 @@ window.Template7 = (function () {
     var t7 = function (template, data) {
         if (arguments.length === 2) {
             var instance = new Template7(template);
-            var rendered = instance.render(data);
+            var rendered = instance.compile()(data);
             instance = null;
             return (rendered);
         }
@@ -393,34 +287,12 @@ window.Template7 = (function () {
     t7.registerHelper = function (name, fn) {
         Template7.prototype.helpers[name] = fn;
     };
-    t7.render = function (template, data) {
-        var instance = t7(template, data);
-    };
+    
     t7.compile = function (template) {
         var instance = new Template7(template);
         return instance.compile();
     };
-    t7.cache = cache;
-    t7.clearCache = function (arg) {
-        if (arguments.length === 0) {
-            cache = {};
-            return;
-        }
-        var key, id;
-        // Clear by passed data
-        for (key in cache) {
-            var lookFor = typeof arg === 'string' ? 'template_' : 'data_';
-            var compareWith = typeof arg === 'string' ? arg : JSON.strigify(arg);
-            if (key.indexOf(lookFor) >= 0 && cache[key] === arg) {
-                id = key.split('_')[1];
-            }
-        }
-        if (id) {
-            delete cache['template_' + id];
-            delete cache['data_' + id];
-            delete cache['rendered_' + id];
-        }
-    };
+    
     t7.options = Template7.prototype.options;
     t7.helpers = Template7.prototype.helpers;
     return t7;
