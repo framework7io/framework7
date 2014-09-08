db17a9a0d57fcac533abbb2f7799efbb1c2477cd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-09-08 19:07:47 +0400

    iOS 8 fix
    Fixes #216

--- src/js/pull-to-refresh.js
+++ src/js/pull-to-refresh.js
@@ -8,7 +8,7 @@ app.initPullToRefresh = function (pageContainer) {
     }
     if (eventsTarget.length === 0) return;
 
-    var isTouched, isMoved, touchesStart = {}, isScrolling, touchesDiff, touchStartTime, container, refresh = false, useTranslate = false, startTranslate = 0, translate, scrollTop;
+    var isTouched, isMoved, touchesStart = {}, isScrolling, touchesDiff, touchStartTime, container, refresh = false, useTranslate = false, startTranslate = 0, translate, scrollTop, wasScrolled;
 
     container = eventsTarget;
 
@@ -22,6 +22,7 @@ app.initPullToRefresh = function (pageContainer) {
         isMoved = false;
         isTouched = true;
         isScrolling = undefined;
+        wasScrolled = undefined;
         touchesStart.x = e.type === 'touchstart' ? e.targetTouches[0].pageX : e.pageX;
         touchesStart.y = e.type === 'touchstart' ? e.targetTouches[0].pageY : e.pageY;
         touchStartTime = (new Date()).getTime();
@@ -42,6 +43,7 @@ app.initPullToRefresh = function (pageContainer) {
         }
 
         scrollTop = container[0].scrollTop;
+        if (typeof wasScrolled === 'undefined' && scrollTop !== 0) wasScrolled = true; 
 
         if (!isMoved) {
             /*jshint validthis:true */
@@ -62,6 +64,9 @@ app.initPullToRefresh = function (pageContainer) {
         touchesDiff = pageY - touchesStart.y;
         
         if (touchesDiff > 0 && scrollTop <= 0 || scrollTop < 0) {
+            // iOS 8 fix
+            if (app.device.os === 'ios' && parseInt(app.device.osVersion.split('.')[0], 10) > 7 && scrollTop === 0 && !wasScrolled) useTranslate = true;
+
             if (useTranslate) {
                 e.preventDefault();
                 translate = (Math.pow(touchesDiff, 0.85) + startTranslate);
