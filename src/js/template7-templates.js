47ff1cc74c1c169b33d7dc86f02a2a423576681b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2014-11-04 13:26:37 +0300

    Fix cache

--- src/js/template7-templates.js
+++ src/js/template7-templates.js
@@ -5,7 +5,7 @@ app.initTemplate7Templates = function () {
     if (!window.Template7) return;
     Template7.templates = Template7.templates || app.params.templates || {};
     Template7.data = Template7.data || app.params.template7Data || {};
-    Template7.template7Cache = {};
+    Template7.cache = Template7.cache || {};
 
     app.templates = Template7.templates;
     app.template7Data = Template7.data;
