6fe56be30f413fed311d1bd71cf6d1baa2eaf654
Joshua Rapp <joshua.rapp@phishme.com>
2014-11-16 23:05:26 -0500

    Up to Snuff
    Getting the App up to snuff with the UI standards.

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -10,6 +10,57 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+//Push Notifications 
+$$(this).on('load', function () {
+	myApp.addNotification({
+		title: 'Spilt Milk',
+		message: 'This is a simple notification message with custom icon and subtitle',
+		media: '<i class="fa fa-cloud fa-2x" style="color: orange;"></i>'
+	});
+});
+
+//Remove Items 
+$$(".fa-times").on('click', function ()  {
+	$$(this).parent().remove();
+});
+
+//Hides notification bubble
+if ($$("div .notification").length > 1) {
+	$$("div .num-notices").show();
+} else {
+	$$("div .num-notices").remove();
+}
+
+// Pull to refresh content
+var ptrContent = $$('.pull-to-refresh-content');
+ 
+// Add 'refresh' listener on it
+ptrContent.on('refresh', function (e) {
+    // Emulate 2s loading
+    setTimeout(function () {
+        // Random image
+        var picURL = 'http://hhhhold.com/88/d/jpg?' + Math.round(Math.random() * 100);
+        // Random song
+        var song = songs[Math.floor(Math.random() * songs.length)];
+        // Random author
+        var author = authors[Math.floor(Math.random() * authors.length)];
+        // List item html
+        var itemHTML = '<li class="item-content">' +
+                          '<div class="item-media"><img src="' + picURL + '" width="44"/></div>' +
+                          '<div class="item-inner">' +
+                            '<div class="item-title-row">' +
+                              '<div class="item-title">' + song + '</div>' +
+                            '</div>' +
+                            '<div class="item-subtitle">' + author + '</div>' +
+                          '</div>' +
+                        '</li>';
+        // Prepend new list element
+        ptrContent.find('ul').prepend(itemHTML);
+        // When loading done, we need to reset it
+        myApp.pullToRefreshDone();
+    }, 2000);
+});
+
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
