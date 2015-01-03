0f7baa65715560ca5a68448be3386468824edb4e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-01-03 23:45:04 +0300

    Picker demos

--- kitchen-sink/js/kitchen-sink.js
+++ kitchen-sink/js/kitchen-sink.js
@@ -448,6 +448,142 @@ myApp.onPageInit('virtual-list', function (page) {
         height: 63,
     });
 });
+/* ===== Pickers ===== */
+myApp.onPageInit('form-pickers', function (page) {
+    var today = new Date();
+
+    // iOS Device picker
+    var pickerDevice = myApp.picker({
+        input: '#ks-picker-device',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+            }
+        ]
+    });
+
+    // Describe yourself picker
+    var pickerDescribe = myApp.picker({
+        input: '#ks-picker-describe',
+        rotateEffect: true,
+        cols: [
+            {
+                textAlign: 'left',
+                values: ('Super Lex Amazing Bat Iron Rocket Lex Cool Beautiful Wonderful Raining Happy Amazing Funny Cool Hot').split(' ')
+            },
+            {
+                values: ('Man Luthor Woman Boy Girl Person Cutie Babe Raccoon').split(' ')
+            },
+        ]
+    });
+
+    // Custom Pickerbar
+    var pickerCustomPickerbar = myApp.picker({
+        input: '#ks-picker-custom-pickerbar',
+        rotateEffect: true,
+        pickerbarHTML: 
+            '<div class="toolbar pickerbar">' +
+                '<div class="left">' +
+                    '<a href="#" class="link custom-pickerbar-random">Randomize</a>' +
+                '</div>' +
+                '<div class="right">' +
+                    '<a href="#" class="link close-picker">That\'s me</a>' +
+                '</div>' +
+            '</div>',
+        cols: [
+            {
+                values: ['Mr', 'Ms'],
+            },
+            {
+                textAlign: 'left',
+                values: ('Super Lex Amazing Bat Iron Rocket Lex Cool Beautiful Wonderful Raining Happy Amazing Funny Cool Hot').split(' ')
+            },
+            {
+                values: ('Man Luthor Woman Boy Girl Person Cutie Babe Raccoon').split(' ')
+            },
+        ],
+        onOpen: function (picker) {
+            picker.container.find('.custom-pickerbar-random').on('click', function () {
+                var col0Values = picker.cols[0].values;
+                var col0Random = col0Values[Math.floor(Math.random() * col0Values.length)];
+
+                var col1Values = picker.cols[1].values;
+                var col1Random = col1Values[Math.floor(Math.random() * col1Values.length)];
+
+                var col2Values = picker.cols[2].values;
+                var col2Random = col2Values[Math.floor(Math.random() * col2Values.length)];
+                
+                picker.setValue([col0Random, col1Random, col2Random]);
+            });
+        }
+    });
+
+    // Inline date-time
+    var pickerInline = myApp.picker({
+        input: '#ks-picker-date',
+        container: '#ks-picker-date-container',
+        pickerbarHTML: '',
+        cssClass: 'ks-date-time-picker',
+        rotateEffect: true,
+        value: [today.getMonth(), today.getDate(), today.getFullYear(), today.getHours(), (today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes())],
+        onChange: function (picker, value, textValue) {
+            var daysInMonth = new Date(picker.value[2], picker.value[0]*1 + 1, 0).getDate();
+            if (value[1] > daysInMonth) {
+                picker.cols[1].setValue(daysInMonth);
+            }
+        },
+        formatValue: function (p, value, textValue) {
+            return textValue[0] + ' ' + value[1] + ', ' + value[2] + ' ' + value[3] + ':' + value[4];
+        },
+        cols: [
+            // Months
+            {
+                values: ('0 1 2 3 4 5 6 7 8 9 10 11').split(' '),
+                textValues: ('January February March April May June July August September October November December').split(' '),
+                textAlign: 'left',
+            },
+            // Days
+            {
+                values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+            },
+            // Years
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 1950; i <= 2030; i++) { arr.push(i); }
+                    return arr;
+                })(),
+            },
+            // Space divider
+            {
+                divider: true,
+                content: '&nbsp;&nbsp;'
+            },
+            // Hours
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 0; i <= 23; i++) { arr.push(i); }
+                    return arr;
+                })(),
+            },
+            // Divider
+            {
+                divider: true,
+                content: ':'
+            },
+            // Minutes
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 0; i <= 59; i++) { arr.push(i < 10 ? '0' + i : i); }
+                    return arr;
+                })(),
+            }
+        ]
+    });
+});
 
 /* ===== Change statusbar bg when panel opened/closed ===== */
 $$('.panel-left').on('open', function () {
