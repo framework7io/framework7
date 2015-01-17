c4804475bfa330cd5593693c5152dd75295ef125
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-01-17 16:40:47 +0300

    Dom7 .add() method

--- src/js/dom7-methods.js
+++ src/js/dom7-methods.js
@@ -637,6 +637,18 @@ Dom7.prototype = {
     },
     detach: function () {
         return this.remove();
+    },
+    add: function () {
+        var dom = this;
+        var i, j;
+        for (i = 0; i < arguments.length; i++) {
+            var toAdd = $(arguments[i]);
+            for (j = 0; j < toAdd.length; j++) {
+                dom[dom.length] = toAdd[j];
+                dom.length++;
+            }
+        }
+        return dom;
     }
 };
 
