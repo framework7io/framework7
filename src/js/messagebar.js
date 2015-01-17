944503d70731c650ddd6bc02690904f02bfec149
Marc Godard <me@marcgodard.com>
2015-01-17 09:24:52 -0500

    requested changes
    Some minor clean up

--- src/js/messagebar.js
+++ src/js/messagebar.js
@@ -24,14 +24,14 @@ app.initMessagebar = function (pageContainer) {
         var height = textarea[0].offsetHeight;
         var diff = height - textarea[0].clientHeight;
         var scrollHeight = textarea[0].scrollHeight;
-        var addExtra = messagebar.attr('data-keyboard-height') ? parseInt(messagebar.attr('data-keyboard-height')) : 0;
+        var addExtra = parseInt((messagebar.attr('data-keyboard-height') || 0), 10);
         // Update
         if (scrollHeight + diff > height) {
             var newAreaHeight = scrollHeight + diff;
             var newBarHeight = initialBarHeight + (newAreaHeight - initialAreaHeight);
             var maxBarHeight = messagebar.attr('data-max-height') || messagebar.parents('.view')[0].offsetHeight - 88;
             if (newBarHeight > maxBarHeight) {
-                newBarHeight = parseInt(maxBarHeight);
+                newBarHeight = parseInt(maxBarHeight, 10);
                 newAreaHeight = newBarHeight - initialBarHeight + initialAreaHeight;
             }
             textarea.css('height', newAreaHeight + 'px');
@@ -44,7 +44,7 @@ app.initMessagebar = function (pageContainer) {
         else {
             if (pageContent.length > 0) {
                 messagebar.css({'height': ''});
-                pageContent.css({'padding-bottom': pageContentInitialPadding + addExtra + 'px'});
+                pageContent.css({'padding-bottom': addExtra ? pageContentInitialPadding + addExtra + 'px' : ''});
             }
         }
     }
