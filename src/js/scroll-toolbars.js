b0f0f7871da1ab0924fd44735870c03d01895057
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-02-06 14:49:51 +0300

    Rename "dont-hide" to "keep"

--- src/js/scroll-toolbars.js
+++ src/js/scroll-toolbars.js
@@ -5,9 +5,9 @@ app.initScrollToolbars = function (pageContainer) {
     pageContainer = $(pageContainer);
     var scrollContent = pageContainer.find('.page-content');
     if (scrollContent.length === 0) return;
-    var hideNavbar = (app.params.hideNavbarOnPageScroll || scrollContent.hasClass('hide-navbar-on-scroll') || scrollContent.hasClass('hide-bars-on-scroll')) && !(scrollContent.hasClass('dont-hide-navbar-on-scroll') || scrollContent.hasClass('dont-hide-bars-on-scroll'));
-    var hideToolbar = (app.params.hideToolbarOnPageScroll || scrollContent.hasClass('hide-toolbar-on-scroll') || scrollContent.hasClass('hide-bars-on-scroll')) && !(scrollContent.hasClass('dont-hide-toolbar-on-scroll') || scrollContent.hasClass('dont-hide-bars-on-scroll'));
-    var hideTabbar = (app.params.hideTabbarOnPageScroll || scrollContent.hasClass('hide-tabbar-on-scroll')) && !(scrollContent.hasClass('dont-hide-tabbar-on-scroll'));
+    var hideNavbar = (app.params.hideNavbarOnPageScroll || scrollContent.hasClass('hide-navbar-on-scroll') || scrollContent.hasClass('hide-bars-on-scroll')) && !(scrollContent.hasClass('keep-navbar-on-scroll') || scrollContent.hasClass('keep-bars-on-scroll'));
+    var hideToolbar = (app.params.hideToolbarOnPageScroll || scrollContent.hasClass('hide-toolbar-on-scroll') || scrollContent.hasClass('hide-bars-on-scroll')) && !(scrollContent.hasClass('keep-toolbar-on-scroll') || scrollContent.hasClass('keep-bars-on-scroll'));
+    var hideTabbar = (app.params.hideTabbarOnPageScroll || scrollContent.hasClass('hide-tabbar-on-scroll')) && !(scrollContent.hasClass('keep-tabbar-on-scroll'));
 
     if (!(hideNavbar || hideToolbar || hideTabbar)) return;
     
