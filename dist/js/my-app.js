4620a05fa2bb8ebd18a94d7d2cc5991dc7e0c333
Joshua Rapp <joshua.rapp@phishme.com>
2015-02-08 23:13:16 -0500

    Cleaned up the item details screen #24129375842669
    Cleaned up the item details screen and included the purchase history at
    the bottom. Still working on on the plotting of the data points.

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -11,25 +11,21 @@ var mainView = myApp.addView('.view-main', {
 });
 
 //Push Notifications 
-$$(this).on('load', function () {
+/*$$(this).on('load', function () {
 	myApp.addNotification({
 		title: 'Spilt Milk',
 		message: 'This is a simple notification message with custom icon and subtitle',
 		media: '<i class="fa fa-cloud fa-2x" style="color: orange;"></i>'
 	});
-});
+});*/
 
 //Remove Items 
-$$(".fa-times").on('click', function ()  {
+$$(".fa-times").on('click', function () {
 	$$(this).parent().remove();
 });
-
+	
 //Hides notification bubble
-if ($$(".panel-right .content-block .notification").length > 1) {
-	$$("div .num-notices").show();
-} else {
-	$$("div .num-notices").remove();
-}
+$$(".empty-message").hide();
 
 //Interaction for the list items
 $$(".checkbox").click(function(){
@@ -39,70 +35,3 @@ $$(".checkbox").click(function(){
 		$$("#item").show();
 	}
 });
-
-// Pull to refresh content
-/*var ptrContent = $$('.pull-to-refresh-content');
- 
-// Add 'refresh' listener on it
-ptrContent.on('refresh', function (e) {
-    // Emulate 2s loading
-    setTimeout(function () {
-        // Random image
-        var picURL = 'http://hhhhold.com/88/d/jpg?' + Math.round(Math.random() * 100);
-        // Random song
-        var song = songs[Math.floor(Math.random() * songs.length)];
-        // Random author
-        var author = authors[Math.floor(Math.random() * authors.length)];
-        // List item html
-        var itemHTML = '<li class="item-content">' +
-                          '<div class="item-media"><img src="' + picURL + '" width="44"/></div>' +
-                          '<div class="item-inner">' +
-                            '<div class="item-title-row">' +
-                              '<div class="item-title">' + song + '</div>' +
-                            '</div>' +
-                            '<div class="item-subtitle">' + author + '</div>' +
-                          '</div>' +
-                        '</li>';
-        // Prepend new list element
-        ptrContent.find('ul').prepend(itemHTML);
-        // When loading done, we need to reset it
-        myApp.pullToRefreshDone();
-    }, 2000);
-});
-
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}*/
