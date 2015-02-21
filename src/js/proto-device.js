66bbb94f80657290a813e470d2ca87c19e9308fe
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-02-21 22:53:48 +0300

    predefine androidChrome

--- src/js/proto-device.js
+++ src/js/proto-device.js
@@ -11,7 +11,7 @@ Framework7.prototype.device = (function () {
     var ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/);
     var iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
 
-    device.ios = device.android = device.iphone = device.ipad = false;
+    device.ios = device.android = device.iphone = device.ipad = device.androidChrome = false;
     
     // Android
     if (android) {
