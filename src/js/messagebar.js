9c45e53153ff78ee7de40ac502d22ea263c7f235
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-03-05 20:45:35 +0300

    Mensageira as component/class

--- src/js/messagebar.js
+++ src/js/messagebar.js
@@ -1,79 +1,137 @@
 /*======================================================
 ************   Messagebar   ************
 ======================================================*/
-app.initMessagebar = function (pageContainer) {
-    pageContainer = $(pageContainer);
-    var messagebar = pageContainer.hasClass('messagebar') ? pageContainer : pageContainer.find('.messagebar');
-    if (messagebar.length === 0) return;
-    var textarea = messagebar.find('textarea');
-    var pageContent = messagebar.parents('.page').find('.page-content');
-    var pageContentInitialPadding = parseInt(pageContent.css('padding-bottom'));
-    var initialBarHeight = messagebar[0].offsetHeight;
-    var initialAreaHeight = textarea[0].offsetHeight;
+var Messagebar = function (container, params) {
+    var defaults = {
+        textarea: null,
+        maxHeight: null,
+    };
+    params = params || {};
+    for (var def in defaults) {
+        if (typeof params[def] === 'undefined' || params[def] === null) {
+            params[def] = defaults[def];
+        }
+    }
+    
+    // Instance
+    var m = this;
 
-    //Prevent submit
+    // Params
+    m.params = params;
+
+    // Container
+    m.container = $(container);
+    if (m.container.length === 0) return;
+
+    // Textarea
+    m.textarea = m.params.textarea ? $(m.params.textarea) : m.container.find('textarea');
+
+    // Is In Page
+    m.pageContainer = m.container.parents('.page').eq(0);
+    m.pageContent = m.pageContainer.find('.page-content');
+
+    // Initial Sizes
+    m.pageContentPadding = parseInt(m.pageContent.css('padding-bottom'));
+    m.initialBarHeight = m.container[0].offsetHeight;
+    m.initialAreaHeight = m.textarea[0].offsetHeight;
+    
+
+    // Resize textarea
+    m.sizeTextarea = function () {
+        // Reset
+        m.textarea.css({'height': ''});
+        
+        var height = m.textarea[0].offsetHeight;
+        var diff = height - m.textarea[0].clientHeight;
+        var scrollHeight = m.textarea[0].scrollHeight;
+
+        // Update
+        if (scrollHeight + diff > height) {
+            var newAreaHeight = scrollHeight + diff;
+            var newBarHeight = m.initialBarHeight + (newAreaHeight - m.initialAreaHeight);
+            var maxBarHeight = m.params.maxHeight || m.container.parents('.view')[0].offsetHeight - 88;
+            if (newBarHeight > maxBarHeight) {
+                newBarHeight = parseInt(maxBarHeight, 10);
+                newAreaHeight = newBarHeight - m.initialBarHeight + m.initialAreaHeight;
+            }
+            m.textarea.css('height', newAreaHeight + 'px');
+            m.container.css('height', newBarHeight + 'px');
+            if (m.pageContent.length > 0) {
+                m.pageContent.css('padding-bottom', newBarHeight + 'px');
+                m.pageContent.scrollTop(m.pageContent[0].scrollHeight - m.pageContent[0].offsetHeight);
+            }
+        }
+        else {
+            if (m.pageContent.length > 0) {
+                m.container.css({'height': '', 'bottom': ''});
+                m.pageContent.css({'padding-bottom': ''});
+            }
+        }
+    };
+    
+    // Clear
+    m.clear = function () {
+        m.textarea.val('').trigger('change');
+    };
+    m.value = function (value) {
+        if (typeof value === 'undefined') return m.textarea.val();
+        else m.textarea.val(value).trigger('change');  
+    };
+    
+    // Handle textarea
+    m.textareaTimeout = undefined;
+    m.handleTextarea = function (e) {
+        clearTimeout(m.textareaTimeout);
+        m.textareaTimeout = setTimeout(function () {
+            m.sizeTextarea();
+        }, 0);
+    };
+
+    //Events
     function preventSubmit(e) {
         e.preventDefault();
     }
 
-    // Resize textarea
-    function sizeTextarea() {
-        // Reset
-        textarea.css({'height': ''});
-        
-        var height = textarea[0].offsetHeight;
-        var diff = height - textarea[0].clientHeight;
-        var scrollHeight = textarea[0].scrollHeight;
-        var addExtra = parseInt((messagebar.attr('data-keyboard-height') || 0), 10);
-        // Update
-        if (scrollHeight + diff > height) {
-            var newAreaHeight = scrollHeight + diff;
-            var newBarHeight = initialBarHeight + (newAreaHeight - initialAreaHeight);
-            var maxBarHeight = messagebar.attr('data-max-height') || messagebar.parents('.view')[0].offsetHeight - 88;
-            if (newBarHeight > maxBarHeight) {
-                newBarHeight = parseInt(maxBarHeight, 10);
-                newAreaHeight = newBarHeight - initialBarHeight + initialAreaHeight;
-            }
-            textarea.css('height', newAreaHeight + 'px');
-            messagebar.css('height', newBarHeight + 'px');
-            if (pageContent.length > 0) {
-                pageContent.css('padding-bottom', newBarHeight + addExtra + 'px');
-                pageContent.scrollTop(pageContent[0].scrollHeight - pageContent[0].offsetHeight);
-            }
-        }
-        else {
-            if (pageContent.length > 0) {
-                messagebar.css({'height': ''});
-                pageContent.css({'padding-bottom': addExtra ? pageContentInitialPadding + addExtra + 'px' : ''});
-            }
-        }
-    }
-    var to;
-    function handleKey(e) {
-        clearTimeout(to);
-        to = setTimeout(function () {
-            sizeTextarea();
-        }, 0);
-            
-    }
-
-    function attachEvents(destroy) {
+    m.attachEvents = function (destroy) {
         var method = destroy ? 'off' : 'on';
-        messagebar[method]('submit', preventSubmit);
-        textarea[method]('change keydown keypress keyup paste cut', handleKey);
-    }
-    function detachEvents() {
-        attachEvents(true);
-    }
+        m.container[method]('submit', preventSubmit);
+        m.textarea[method]('change keydown keypress keyup paste cut', m.handleTextarea);
+    };
+    m.detachEvents = function () {
+        m.attachEvents(true);
+    };
     
-    messagebar[0].f7DestroyMessagebar = detachEvents;
+    // Init Destroy
+    m.init = function () {
+        m.attachEvents();
+    };
+    m.destroy = function () {
+        m.detachEvents();
+        m = null;
+    };
 
-    // Attach events
-    attachEvents();
+    // Init
+    m.init();
+
+    m.container[0].f7Searchbar = m;
+    return m;
+};
+app.messagebar = function (container, params) {
+    return new Messagebar(container, params);
+};
+app.initPageMessagebar = function (pageContainer) {
+    pageContainer = $(pageContainer);
+    var messagebar = pageContainer.hasClass('messagebar') ? pageContainer : pageContainer.find('.messagebar');
+    if (messagebar.length === 0) return;
+    if (!messagebar.hasClass('messagebar-init')) return;
+    var mb = app.messagebar(messagebar, {
+        textarea: messagebar.attr('data-textarea'),
+        maxHeight: messagebar.attr('data-max-height') && parseInt(messagebar.attr('data-max-height'), 10),
+    });
 
     // Destroy on page remove
     function pageBeforeRemove() {
-        detachEvents();
+        mb.destroy();
         pageContainer.off('pageBeforeRemove', pageBeforeRemove);
     }
     if (pageContainer.hasClass('page')) {
@@ -84,5 +142,5 @@ app.destroyMessagebar = function (pageContainer) {
     pageContainer = $(pageContainer);
     var messagebar = pageContainer.hasClass('messagebar') ? pageContainer : pageContainer.find('.messagebar');
     if (messagebar.length === 0) return;
-    if (messagebar[0].f7DestroyMessagebar) messagebar[0].f7DestroyMessagebar();
+    if (messagebar[0].f7Messagebar) messagebar[0].f7Messagebar.destroy();
 };
