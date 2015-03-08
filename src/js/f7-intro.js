c2c05e5d7890ab2041ab1b2b037be7f1d228df82
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-03-08 13:39:26 +0300

    Fix version

--- src/js/f7-intro.js
+++ src/js/f7-intro.js
@@ -8,7 +8,7 @@ window.Framework7 = function (params) {
     var app = this;
 
     // Version
-    app.version = '1.0.2';
+    app.version = '1.0.3';
 
     // Default Parameters
     app.params = {
