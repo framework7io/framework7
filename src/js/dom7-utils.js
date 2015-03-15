6901a64e7dd14f086ddfc2dc283660df3d1649b7
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-03-15 20:20:05 +0300

    Add $.each method

--- src/js/dom7-utils.js
+++ src/js/dom7-utils.js
@@ -14,6 +14,25 @@ $.isArray = function (arr) {
     if (Object.prototype.toString.apply(arr) === '[object Array]') return true;
     else return false;
 };
+$.each = function (obj, callback) {
+    if (typeof obj !== 'object') return;
+    if (!callback) return;
+    var i, prop;
+    if ($.isArray(obj) || obj instanceof Dom7) {
+        // Array
+        for (i = 0; i < obj.length; i++) {
+            callback(i, obj[i]);
+        }
+    }
+    else {
+        // Object
+        for (prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                callback(prop, obj[prop]);
+            }
+        }
+    }
+};
 $.unique = function (arr) {
     var unique = [];
     for (var i = 0; i < arr.length; i++) {
