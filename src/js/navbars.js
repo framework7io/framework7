18dcdc5772aa88c4c6e6957c84672c158f6978b7
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-04-29 14:28:27 +0300

    Better look up for a page

--- src/js/navbars.js
+++ src/js/navbars.js
@@ -28,7 +28,6 @@ app.navbarInitCallback = function (view, pageContainer, navbarContainer, navbarI
         return;
     }
     navbarInnerContainer.f7NavbarInitialized = true;
-
     // Before Init
     app.pluginHook('navbarBeforeInit', navbarData, pageData);
     $(navbarInnerContainer).trigger('navbarBeforeInit', eventData);
@@ -68,17 +67,29 @@ app.initNavbarWithCallback = function (navbarContainer) {
     var viewContainer = navbarContainer.parents('.' + app.params.viewClass);
     var view;
     if (viewContainer.length === 0) return;
+    if (navbarContainer.parents('.navbar-through').length === 0 && viewContainer.find('.navbar-through').length === 0) return;
     view = viewContainer[0].f7View || undefined;
 
     navbarContainer.find('.navbar-inner').each(function () {
         var navbarInnerContainer = this;
         var pageContainer;
-        viewContainer.find('.page').each(function () {
-            if (this.f7PageData && this.f7PageData.navbarInnerContainer === navbarInnerContainer) {
-                pageContainer = this;
-
+        if ($(navbarInnerContainer).attr('data-page')) {
+            // For dom cache
+            pageContainer = viewContainer.find('.page[data-page="' + $(navbarInnerContainer).attr('data-page') + '"]')[0];
+        }
+        if (!pageContainer) {
+            var pages = viewContainer.find('.page');
+            if (pages.length === 1) {
+                pageContainer = pages[0];
             }
-        });
+            else {
+                viewContainer.find('.page').each(function () {
+                    if (this.f7PageData && this.f7PageData.navbarInnerContainer === navbarInnerContainer) {
+                        pageContainer = this;
+                    }
+                });
+            }
+        }
         app.navbarInitCallback(view, pageContainer, navbarContainer[0], navbarInnerContainer);
     });
 };
