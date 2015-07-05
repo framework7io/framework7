98b8360b9df5c346bad1fbce1b3f431b4440c7db
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-07-05 15:36:21 +0300

    Remove ripple effect from swipeout actions

--- src/js/f7-intro.js
+++ src/js/f7-intro.js
@@ -3,12 +3,11 @@
 Framework 7
 ===========================*/
 window.Framework7 = function (params) {
-
     // App
     var app = this;
 
     // Version
-    app.version = '1.0.7';
+    app.version = '1.2.0';
 
     // Default Parameters
     app.params = {
@@ -113,7 +112,7 @@ window.Framework7 = function (params) {
         material: false,
         materialPreloaderSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="75" width="75" viewbox="0 0 75 75"><circle cx="37.5" cy="37.5" r="33.5" stroke-width="8"/></svg>',
         materialRipple: true,
-        materialRippleElements: '.ripple, a.link, a.item-link, .button, .modal-button, .tab-link, .swipeout-actions-right a, .swipeout-actions-left a, .label-radio, .label-checkbox, .actions-modal-button, a.searchbar-clear, .picker-calendar-day span',
+        materialRippleElements: '.ripple, a.link, a.item-link, .button, .modal-button, .tab-link, .label-radio, .label-checkbox, .actions-modal-button, a.searchbar-clear, .floating-button',
         // Auto init
         init: true,
     };
