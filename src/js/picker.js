c62e637c4d4500e1a2a9b8eea5f3e693948f7bea
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-07-20 15:41:12 +0800

    speedup picker on android

--- src/js/picker.js
+++ src/js/picker.js
@@ -193,13 +193,12 @@ var Picker = function (params) {
             if (activeIndex >= col.items.length) activeIndex = col.items.length - 1;
             var previousActiveIndex = col.activeIndex;
             col.activeIndex = activeIndex;
-            col.wrapper.find('.picker-selected, .picker-after-selected, .picker-before-selected').removeClass('picker-selected picker-after-selected picker-before-selected');
-
             col.items.transition(transition);
+            col.wrapper.find('.picker-selected').removeClass('picker-selected');
+            if (p.params.rotateEffect) {
+              col.items.transition(transition);
+            }
             var selectedItem = col.items.eq(activeIndex).addClass('picker-selected').transform('');
-            var prevItems = selectedItem.prevAll().addClass('picker-before-selected');
-            var nextItems = selectedItem.nextAll().addClass('picker-after-selected');
-                
             // Set 3D rotate effect
             if (p.params.rotateEffect) {
                 var percentage = (translate - (Math.floor((translate - maxTranslate)/itemHeight) * itemHeight + maxTranslate)) / itemHeight;
