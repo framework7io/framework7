802b93ac41eec71d2535a920e9bb1a3448d3d328
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-08-09 19:19:10 +0300

    Extend shortcuts + check element data first
    Fixes #546

--- src/js/dom7-methods.js
+++ src/js/dom7-methods.js
@@ -91,10 +91,16 @@ Dom7.prototype = {
         if (typeof value === 'undefined') {
             // Get value
             if (this[0]) {
-                var dataKey = this[0].getAttribute('data-' + key);
-                if (dataKey) return dataKey;
-                else if (this[0].dom7ElementDataStorage && (key in this[0].dom7ElementDataStorage)) return this[0].dom7ElementDataStorage[key];
-                else return undefined;
+                if (this[0].dom7ElementDataStorage && (key in this[0].dom7ElementDataStorage)) {
+                    return this[0].dom7ElementDataStorage[key];
+                }
+                else {
+                    var dataKey = this[0].getAttribute('data-' + key);    
+                    if (dataKey) {
+                        return dataKey;
+                    }
+                    else return undefined;
+                }
             }
             else return undefined;
         }
@@ -714,9 +720,9 @@ Dom7.prototype = {
     var shortcuts = ('click blur focus focusin focusout keyup keydown keypress submit change mousedown mousemove mouseup mouseenter mouseleave mouseout mouseover touchstart touchend touchmove resize scroll').split(' ');
     var notTrigger = ('resize scroll').split(' ');
     function createMethod(name) {
-        Dom7.prototype[name] = function (handler) {
+        Dom7.prototype[name] = function (targetSelector, listener, capture) {
             var i;
-            if (typeof handler === 'undefined') {
+            if (typeof targetSelector === 'undefined') {
                 for (i = 0; i < this.length; i++) {
                     if (notTrigger.indexOf(name) < 0) {
                         if (name in this[i]) this[i][name]();
@@ -728,7 +734,7 @@ Dom7.prototype = {
                 return this;
             }
             else {
-                return this.on(name, handler);
+                return this.on(name, targetSelector, listener, capture);
             }
         };
     }
