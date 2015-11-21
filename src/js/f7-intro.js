7e82c5b6f918f0e8ed6232cca2c79982010ad8f7
iamxiaoma <395487290@qq.com>
2015-11-21 13:12:00 +0800

    framrwork7 for webapp
    update the name and version

--- src/js/f7-intro.js
+++ src/js/f7-intro.js
@@ -7,7 +7,7 @@ window.Framework7 = function (params) {
     var app = this;
 
     // Version
-    app.version = '1.3.5';
+    app.version = '1.3.6';
 
     // Default Parameters
     app.params = {
