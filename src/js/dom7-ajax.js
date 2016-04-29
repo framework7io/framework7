1e5c6d5d1155a0c778bcbf7019bab2cf22c31347
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-04-29 19:40:26 +0300

    Add error method for XHR shortcuts
    Fixes #890

--- src/js/dom7-ajax.js
+++ src/js/dom7-ajax.js
@@ -264,12 +264,13 @@ $.ajax = function (options) {
 (function () {
     var methods = ('get post getJSON').split(' ');
     function createMethod(method) {
-        $[method] = function (url, data, success) {
+        $[method] = function (url, data, success, error) {
             return $.ajax({
                 url: url,
                 method: method === 'post' ? 'POST' : 'GET',
                 data: typeof data === 'function' ? undefined : data,
                 success: typeof data === 'function' ? data : success,
+                error: typeof data === 'function' ? success : error,
                 dataType: method === 'getJSON' ? 'json' : undefined
             });
         };
