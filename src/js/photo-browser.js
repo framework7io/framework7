a5c3a80fd6191353e4c9cfe6aa4e86a2cd678c46
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-16 19:44:16 +0300

    Move zoom logic to Swiper

--- src/js/photo-browser.js
+++ src/js/photo-browser.js
@@ -10,8 +10,8 @@ var PhotoBrowser = function (params) {
         spaceBetween: 20,
         speed: 300,
         zoom: true,
-        maxZoom: 3,
-        minZoom: 1,
+        zoomMax: 3,
+        zoomMin: 1,
         exposition: true,
         expositionHideCaptions: false,
         type: 'standalone',
@@ -44,13 +44,19 @@ var PhotoBrowser = function (params) {
         onSwipeToClose(pb)
         */
     };
-    
+
     params = params || {};
     if (!params.backLinkText && app.params.material) defaults.backLinkText = '';
     for (var def in defaults) {
         if (typeof params[def] === 'undefined') {
             params[def] = defaults[def];
         }
+    }
+    if (params.maxZoom) {
+        params.zoomMax = params.maxZoom;
+    }
+    if (params.minZoom) {
+        params.zoomMin = params.minZoom;
     }
 
     pb.params = params;
@@ -60,14 +66,14 @@ var PhotoBrowser = function (params) {
     // Templates
     var photoTemplate = pb.params.photoTemplate || 
         '<div class="photo-browser-slide swiper-slide">' +
-            '<span class="photo-browser-zoom-container">' +
+            '<span class="swiper-zoom-container">' +
                 '<img src="{{js "this.url || this"}}">' +
             '</span>' +
         '</div>';
     var photoLazyTemplate = pb.params.lazyPhotoTemplate ||
         '<div class="photo-browser-slide photo-browser-slide-lazy swiper-slide">' +
             '<div class="preloader {{@root.preloaderColorClass}}">{{#if @root.material}}{{@root.materialPreloaderHtml}}{{/if}}</div>' +
-            '<span class="photo-browser-zoom-container">' +
+            '<span class="swiper-zoom-container">' +
                 '<img data-src="{{js "this.url || this"}}" class="swiper-lazy">' +
             '</span>' +
         '</div>';
@@ -194,7 +200,7 @@ var PhotoBrowser = function (params) {
         // Destroy slider
         pb.swiper.destroy();
         // Delete references
-        pb.swiper = pb.swiperContainer = pb.swiperWrapper = pb.slides = gestureSlide = gestureImg = gestureImgWrap = undefined;
+        pb.swiper = pb.swiperContainer = pb.swiperWrapper = pb.slides = undefined; //gestureSlide = gestureImg = gestureImgWrap = undefined;
     };
 
     pb.onPopupClose = function (e) {
@@ -229,7 +235,7 @@ var PhotoBrowser = function (params) {
         pb.container.find('.photo-browser-total').text(total);
 
         $('.photo-browser-prev, .photo-browser-next').removeClass('photo-browser-link-inactive');
-        
+
         if (swiper.isBeginning && !pb.params.loop) {
             $('.photo-browser-prev').addClass('photo-browser-link-inactive');
         }
@@ -254,16 +260,9 @@ var PhotoBrowser = function (params) {
         if (pb.params.onTransitionStart) pb.params.onTransitionStart(swiper);
     };
     pb.onSliderTransitionEnd = function (swiper) {
-        // Reset zoom
-        if (pb.params.zoom && gestureSlide && swiper.previousIndex !== swiper.activeIndex) {
-            gestureImg.transform('translate3d(0,0,0) scale(1)');
-            gestureImgWrap.transform('translate3d(0,0,0)');
-            gestureSlide = gestureImg = gestureImgWrap = undefined;
-            scale = currentScale = 1;
-        }
         if (pb.params.onTransitionEnd) pb.params.onTransitionEnd(swiper);
     };
-    
+
     pb.layout = function (index) {
         if (pb.params.type === 'page') {
             pb.container = $('.photo-browser-swiper-container').parents('.view');
@@ -280,7 +279,7 @@ var PhotoBrowser = function (params) {
         pb.slides = pb.container.find('.photo-browser-slide');
         pb.captionsContainer = pb.container.find('.photo-browser-captions');
         pb.captions = pb.container.find('.photo-browser-caption');
-        
+
         var sliderSettings = {
             nextButton: pb.params.nextButton || '.photo-browser-next',
             prevButton: pb.params.prevButton || '.photo-browser-prev',
@@ -293,6 +292,9 @@ var PhotoBrowser = function (params) {
             lazyLoadingInPrevNext: pb.params.lazyLoadingInPrevNext,
             lazyLoadingOnTransitionStart: pb.params.lazyLoadingOnTransitionStart,
             preloadImages: pb.params.lazyLoading ? false : true,
+            zoom: pb.params.zoom,
+            zoomMax: pb.params.zoomMax,
+            zoomMin: pb.params.zoomMin,
             onTap: function (swiper, e) {
                 if (pb.params.onTap) pb.params.onTap(swiper, e);
             },
@@ -301,14 +303,14 @@ var PhotoBrowser = function (params) {
                 if (pb.params.onClick) pb.params.onClick(swiper, e);
             },
             onDoubleTap: function (swiper, e) {
-                pb.toggleZoom(e);
+                // pb.toggleZoom(e);
                 if (pb.params.onDoubleTap) pb.params.onDoubleTap(swiper, e);
             },
             onTransitionStart: function (swiper) {
                 pb.onSliderTransitionStart(swiper);
             },
             onTransitionEnd: function (swiper) {
-                pb.onSliderTransitionEnd(swiper);  
+                pb.onSliderTransitionEnd(swiper);
             },
             onSlideChangeStart: pb.params.onSlideChangeStart,
             onSlideChangeEnd: pb.params.onSlideChangeEnd,
@@ -335,25 +337,9 @@ var PhotoBrowser = function (params) {
     };
     pb.attachEvents = function (detach) {
         var action = detach ? 'off' : 'on';
-        // Slide between photos
-
-        if (pb.params.zoom) {
-            var target = pb.params.loop ? pb.swiper.slides : pb.slides;
-            // Scale image
-            target[action]('gesturestart', pb.onSlideGestureStart);
-            target[action]('gesturechange', pb.onSlideGestureChange);
-            target[action]('gestureend', pb.onSlideGestureEnd);
-
-            // Move image
-            target[action](app.touchEvents.start, pb.onSlideTouchStart);
-            target[action](app.touchEvents.move, pb.onSlideTouchMove);
-            target[action](app.touchEvents.end, pb.onSlideTouchEnd);
-        }
         pb.container.find('.photo-browser-close-link')[action]('click', pb.close);
     };
 
-    var isTouched, isMoved, touchesStart = {}, touchesCurrent = {}, touchStartTime, isScrolling, animating = false, currentTranslate;
-    var allowClick = true;
 
     // Expose
     pb.exposed = false;
@@ -371,228 +357,6 @@ var PhotoBrowser = function (params) {
         if (pb.container) pb.container.removeClass('photo-browser-exposed');
         if (pb.params.expositionHideCaptions) pb.captionsContainer.removeClass('photo-browser-captions-exposed');
         pb.exposed = false;
-    };
-    
-    // Gestures
-    var gestureSlide, gestureImg, gestureImgWrap, scale = 1, currentScale = 1, isScaling = false;
-    pb.onSlideGestureStart = function (e) {
-        if (!gestureSlide || !gestureSlide.length) {
-            gestureSlide = $(this);
-            if (gestureSlide.length === 0) gestureSlide = pb.swiper.slides.eq(pb.swiper.activeIndex);
-            gestureImg = gestureSlide.find('img, svg, canvas');
-            gestureImgWrap = gestureImg.parent('.photo-browser-zoom-container');
-            if (gestureImgWrap.length === 0) {
-                gestureImg = undefined;
-                return;
-            }
-        }
-        gestureImg.transition(0);
-        isScaling = true;
-    };
-    pb.onSlideGestureChange = function (e) {
-        if (!gestureImg || gestureImg.length === 0) return;
-        scale = e.scale * currentScale;
-        if (scale > pb.params.maxZoom) {
-            scale = pb.params.maxZoom - 1 + Math.pow((scale - pb.params.maxZoom + 1), 0.5);
-        }
-        if (scale < pb.params.minZoom) {
-            scale =  pb.params.minZoom + 1 - Math.pow((pb.params.minZoom - scale + 1), 0.5);
-        }
-        gestureImg.transform('translate3d(0,0,0) scale(' + scale + ')');
-    };
-    pb.onSlideGestureEnd = function (e) {
-        if (!gestureImg || gestureImg.length === 0) return;
-        scale = Math.max(Math.min(scale, pb.params.maxZoom), pb.params.minZoom);
-        gestureImg.transition(pb.params.speed).transform('translate3d(0,0,0) scale(' + scale + ')');
-        currentScale = scale;
-        isScaling = false;
-        if (scale === 1) gestureSlide = undefined;
-    };
-    pb.toggleZoom = function (e) {
-        if (!gestureSlide) {
-            gestureSlide = pb.swiper.slides.eq(pb.swiper.activeIndex);
-            gestureImg = gestureSlide.find('img, svg, canvas');
-            gestureImgWrap = gestureImg.parent('.photo-browser-zoom-container');
-        }
-        if (!gestureImg || gestureImg.length === 0) return;
-        
-        var touchX, touchY, offsetX, offsetY, diffX, diffY, translateX, translateY, imageWidth, imageHeight, scaledWidth, scaledHeight, translateMinX, translateMinY, translateMaxX, translateMaxY;
-
-        if (typeof imageTouchesStart.x === 'undefined' && e) {
-            touchX = e.type === 'touchend' ? e.changedTouches[0].pageX : e.pageX;
-            touchY = e.type === 'touchend' ? e.changedTouches[0].pageY : e.pageY;
-        }
-        else {
-            touchX = imageTouchesStart.x;
-            touchY = imageTouchesStart.y;
-        }
-        
-        if (scale && scale !== 1) {
-            // Zoom Out
-            scale = currentScale = 1;
-            gestureImgWrap.transition(300).transform('translate3d(0,0,0)');
-            gestureImg.transition(300).transform('translate3d(0,0,0) scale(1)');
-            gestureSlide = undefined;
-        }
-        else {
-            // Zoom In
-            scale = currentScale = pb.params.maxZoom;
-            if (e) {
-                offsetX = pb.container.offset().left;
-                offsetY = pb.container.offset().top;
-                diffX = offsetX + pb.container[0].offsetWidth/2 - touchX;
-                diffY = offsetY + pb.container[0].offsetHeight/2 - touchY;
-
-                imageWidth = gestureImg[0].offsetWidth;
-                imageHeight = gestureImg[0].offsetHeight;
-                scaledWidth = imageWidth * scale;
-                scaledHeight = imageHeight * scale;
-
-                translateMinX = Math.min((pb.swiper.width / 2 - scaledWidth / 2), 0);
-                translateMinY = Math.min((pb.swiper.height / 2 - scaledHeight / 2), 0);
-                translateMaxX = -translateMinX;
-                translateMaxY = -translateMinY;
-
-                translateX = diffX * scale;
-                translateY = diffY * scale;
-                
-                if (translateX < translateMinX) {
-                    translateX =  translateMinX;
-                }
-                if (translateX > translateMaxX) {
-                    translateX = translateMaxX;
-                }
-                
-                if (translateY < translateMinY) {
-                    translateY =  translateMinY;
-                }
-                if (translateY > translateMaxY) {
-                    translateY = translateMaxY;
-                }
-            }
-            else {
-                translateX = 0;
-                translateY = 0;
-            }
-            gestureImgWrap.transition(300).transform('translate3d(' + translateX + 'px, ' + translateY + 'px,0)');
-            gestureImg.transition(300).transform('translate3d(0,0,0) scale(' + scale + ')');
-        }
-    };
-
-    var imageIsTouched, imageIsMoved, imageCurrentX, imageCurrentY, imageMinX, imageMinY, imageMaxX, imageMaxY, imageWidth, imageHeight, imageTouchesStart = {}, imageTouchesCurrent = {}, imageStartX, imageStartY, velocityPrevPositionX, velocityPrevTime, velocityX, velocityPrevPositionY, velocityY;
-
-    pb.onSlideTouchStart = function (e) {
-        if (!gestureImg || gestureImg.length === 0) return;
-        if (imageIsTouched) return;
-        if (app.device.os === 'android') e.preventDefault();
-        imageIsTouched = true;
-        imageTouchesStart.x = e.type === 'touchstart' ? e.targetTouches[0].pageX : e.pageX;
-        imageTouchesStart.y = e.type === 'touchstart' ? e.targetTouches[0].pageY : e.pageY;
-    };
-    pb.onSlideTouchMove = function (e) {
-        if (!gestureImg || gestureImg.length === 0) return;
-        pb.swiper.allowClick = false;
-        if (!imageIsTouched || !gestureSlide) return;
-
-        if (!imageIsMoved) {
-            imageWidth = gestureImg[0].offsetWidth;
-            imageHeight = gestureImg[0].offsetHeight;
-            imageStartX = $.getTranslate(gestureImgWrap[0], 'x') || 0;
-            imageStartY = $.getTranslate(gestureImgWrap[0], 'y') || 0;
-            gestureImgWrap.transition(0);
-        }
-        // Define if we need image drag
-        var scaledWidth = imageWidth * scale;
-        var scaledHeight = imageHeight * scale;
-
-        if (scaledWidth < pb.swiper.width && scaledHeight < pb.swiper.height) return;
-
-        imageMinX = Math.min((pb.swiper.width / 2 - scaledWidth / 2), 0);
-        imageMaxX = -imageMinX;
-        imageMinY = Math.min((pb.swiper.height / 2 - scaledHeight / 2), 0);
-        imageMaxY = -imageMinY;
-        
-        imageTouchesCurrent.x = e.type === 'touchmove' ? e.targetTouches[0].pageX : e.pageX;
-        imageTouchesCurrent.y = e.type === 'touchmove' ? e.targetTouches[0].pageY : e.pageY;
-
-        if (!imageIsMoved && !isScaling) {
-            if (
-                (Math.floor(imageMinX) === Math.floor(imageStartX) && imageTouchesCurrent.x < imageTouchesStart.x) ||
-                (Math.floor(imageMaxX) === Math.floor(imageStartX) && imageTouchesCurrent.x > imageTouchesStart.x)
-                ) {
-                imageIsTouched = false;
-                return;
-            }
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        imageIsMoved = true;
-        imageCurrentX = imageTouchesCurrent.x - imageTouchesStart.x + imageStartX;
-        imageCurrentY = imageTouchesCurrent.y - imageTouchesStart.y + imageStartY;
-        
-        if (imageCurrentX < imageMinX) {
-            imageCurrentX =  imageMinX + 1 - Math.pow((imageMinX - imageCurrentX + 1), 0.8);
-        }
-        if (imageCurrentX > imageMaxX) {
-            imageCurrentX = imageMaxX - 1 + Math.pow((imageCurrentX - imageMaxX + 1), 0.8);
-        }
-        
-        if (imageCurrentY < imageMinY) {
-            imageCurrentY =  imageMinY + 1 - Math.pow((imageMinY - imageCurrentY + 1), 0.8);
-        }
-        if (imageCurrentY > imageMaxY) {
-            imageCurrentY = imageMaxY - 1 + Math.pow((imageCurrentY - imageMaxY + 1), 0.8);
-        }
-
-        //Velocity
-        if (!velocityPrevPositionX) velocityPrevPositionX = imageTouchesCurrent.x;
-        if (!velocityPrevPositionY) velocityPrevPositionY = imageTouchesCurrent.y;
-        if (!velocityPrevTime) velocityPrevTime = Date.now();
-        velocityX = (imageTouchesCurrent.x - velocityPrevPositionX) / (Date.now() - velocityPrevTime) / 2;
-        velocityY = (imageTouchesCurrent.y - velocityPrevPositionY) / (Date.now() - velocityPrevTime) / 2;
-        if (Math.abs(imageTouchesCurrent.x - velocityPrevPositionX) < 2) velocityX = 0;
-        if (Math.abs(imageTouchesCurrent.y - velocityPrevPositionY) < 2) velocityY = 0;
-        velocityPrevPositionX = imageTouchesCurrent.x;
-        velocityPrevPositionY = imageTouchesCurrent.y;
-        velocityPrevTime = Date.now();
-
-        gestureImgWrap.transform('translate3d(' + imageCurrentX + 'px, ' + imageCurrentY + 'px,0)');
-    };
-    pb.onSlideTouchEnd = function (e) {
-        if (!gestureImg || gestureImg.length === 0) return;
-        if (!imageIsTouched || !imageIsMoved) {
-            imageIsTouched = false;
-            imageIsMoved = false;
-            return;
-        }
-        imageIsTouched = false;
-        imageIsMoved = false;
-        var momentumDurationX = 300;
-        var momentumDurationY = 300;
-        var momentumDistanceX = velocityX * momentumDurationX;
-        var newPositionX = imageCurrentX + momentumDistanceX;
-        var momentumDistanceY = velocityY * momentumDurationY;
-        var newPositionY = imageCurrentY + momentumDistanceY;
-
-        //Fix duration
-        if (velocityX !== 0) momentumDurationX = Math.abs((newPositionX - imageCurrentX) / velocityX);
-        if (velocityY !== 0) momentumDurationY = Math.abs((newPositionY - imageCurrentY) / velocityY);
-        var momentumDuration = Math.max(momentumDurationX, momentumDurationY);
-
-        imageCurrentX = newPositionX;
-        imageCurrentY = newPositionY;
-
-        // Define if we need image drag
-        var scaledWidth = imageWidth * scale;
-        var scaledHeight = imageHeight * scale;
-        imageMinX = Math.min((pb.swiper.width / 2 - scaledWidth / 2), 0);
-        imageMaxX = -imageMinX;
-        imageMinY = Math.min((pb.swiper.height / 2 - scaledHeight / 2), 0);
-        imageMaxY = -imageMinY;
-        imageCurrentX = Math.max(Math.min(imageCurrentX, imageMaxX), imageMinX);
-        imageCurrentY = Math.max(Math.min(imageCurrentY, imageMaxY), imageMinY);
-
-        gestureImgWrap.transition(momentumDuration).transform('translate3d(' + imageCurrentX + 'px, ' + imageCurrentY + 'px,0)');
     };
 
     // Swipe Up To Close
