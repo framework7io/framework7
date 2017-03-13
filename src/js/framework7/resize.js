6ba568da07455fb04605ea284a7f3c07f9a1c116
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-03-13 15:15:32 +0300

    app size

--- src/js/framework7/resize.js
+++ src/js/framework7/resize.js
@@ -10,18 +10,26 @@ function _fixIpadBodyScrolLeft() {
         }, 0);
     }
 }
-app.initResize = function () {
-    $(window).on('resize', app.resize);
-    $(window).on('orientationchange', app.orientationchange);
-};
+function _getAppSize() {
+    var offset = app.root.offset();
+    app.width = app.root[0].offsetWidth;
+    app.height = app.root[0].offsetHeight;
+    app.left = offset.left;
+    app.top = offset.top;
+}
 app.resize = function () {
     if (app.sizeNavbars) app.sizeNavbars();
     _fixIpadBodyScrolLeft();
-    
+    _getAppSize();
 };
 app.orientationchange = function () {
     if (app.device && app.device.minimalUi) {
         if (window.orientation === 90 || window.orientation === -90) document.body.scrollTop = 0;
     }
     _fixIpadBodyScrolLeft();
+};
+app.initResize = function () {
+    $(window).on('resize', app.resize);
+    $(window).on('orientationchange', app.orientationchange);
+    _getAppSize();
 };
