b46a2bb258f808a7bf56a854f2663d0710e6e00a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-26 16:05:08 +0300

    Use util

---
+++ src/js/utils/use.js
@@ -0,0 +1,30 @@
+import Utils from './utils';
+
+export default function Use(Class) {
+  Class.use = function use(module, ...params) {
+    const name = module.name || Utils.now();
+    if (Class.prototype.modules) {
+      Class.prototype.modules[name] = module;
+    }
+    // Prototype
+    if (module.proto) {
+      Object.keys(module.proto).forEach((key) => {
+        Class.prototype[key] = module.proto[key];
+      });
+    }
+    // Class
+    if (module.class) {
+      Object.keys(module.class).forEach((key) => {
+        Class[key] = module.class[key];
+      });
+    }
+    // Callback
+    if (module.install) {
+      module.install.apply(Class, params);
+    }
+    return Class;
+  }
+  return Class;
+}
+
+// export default Use;
