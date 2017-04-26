e9a9eb578d8f8f8ad0b463bf49459c2bc15be862
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-26 16:01:35 +0300

    Router class

---
+++ src/js/classes/router.js
@@ -0,0 +1,53 @@
+import Use from '../utils/use';
+import Events from '../modules/events';
+
+class Router {
+  constructor(app, view) {
+    const router = this;
+
+    router.navigate = function navigate(url) {
+
+    };
+    /*
+    Router load, back params:
+      reloadAll: Boolean,
+      reloadCurrent: Boolean
+      reloadPrevious: Boolean,
+      component: Object,
+      effect: String,
+      animate: Boolean,
+      content: String,
+      template: String/Function,
+      templateContext: Object,
+      templateContextName: String,
+      name: String, // page name
+      pushState: Boolean,
+      ignoreCache: Boolean,
+      force: Boolean,
+      element: HTMLElement,
+      query: Object,
+      url: String, //to load via Ajax
+
+      url
+      content
+      component
+      name
+      element
+      template
+    */
+    router.load = function load(params) {
+
+    };
+    router.back = function back(params) {
+
+    };
+
+    return router;
+  }
+}
+
+// Use Events
+Use(Router).use(Events);
+
+export default Router;
+
