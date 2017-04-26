5bf6b7d3da6c1401a9e88e5c92e8b9355bbe12cc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-26 16:02:14 +0300

    Move events, add once and event parents

---
+++ src/js/modules/events.js
@@ -0,0 +1,47 @@
+export default {
+  name: 'events',
+  install(eventsParent) {
+    this.prototype.eventsListeners = {};
+    if (eventsParent) this.prototype.eventsParent = eventsParent;
+  },
+  proto: {
+    on(event, handler) {
+      const self = this;
+      if (!self.eventsListeners[event]) self.eventsListeners[event] = [];
+      self.eventsListeners[event].push(handler);
+      return self;
+    },
+    once(event, handler) {
+      const self = this;
+      function onceHandler(...args) {
+        handler.apply(self, args);
+        self.off(event, onceHandler);
+      }
+      self.on(event, onceHandler);
+    },
+    off(event, handler) {
+      const self = this;
+      if (typeof handler === 'undefined') {
+        self.eventsListeners[event] = [];
+        return self;
+      }
+      self.eventsListeners[event].forEach((eventHandler, index) => {
+        if (eventHandler === handler) {
+          self.eventsListeners[event].splice(index, 1);
+        }
+      });
+      return self;
+    },
+    emit(event, ...args) {
+      const self = this;
+      if (self.eventsParent && self.eventsParent.prototype) {
+        self.eventsParent.prototype.emit(event, ...args);
+      }
+      if (!self.eventsListeners[event]) return self;
+      self.eventsListeners[event].forEach((eventHandler) => {
+        eventHandler.apply(self, args);
+      });
+      return self;
+    },
+  },
+};
