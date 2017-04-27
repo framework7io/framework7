096bf493ab07ea93f2a4f347189a5eb6a3a6278e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-27 18:18:39 +0300

    add more parameters

--- src/js/modules/view.js
+++ src/js/modules/view.js
@@ -1,4 +1,4 @@
-import View from '../classes/view';
+import View from '../classes/view-class';
 
 export default {
   name: 'view',
@@ -6,6 +6,12 @@ export default {
     viewClass: 'view',
     viewMainClass: 'view-main',
     viewsClass: 'views',
+    // Swipe Back
+    swipeBackPage: true,
+    swipeBackPageAnimateShadow: true,
+    swipeBackPageAnimateOpacity: true,
+    swipeBackPageActiveArea: 30,
+    swipeBackPageThreshold: 0,
   },
   class: {
     View,
