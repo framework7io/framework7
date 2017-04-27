dc48b72292b1bd8914935ba29fc49bf33c22dd4b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-27 18:18:56 +0300

    Add more modules

--- src/js/framework7.js
+++ src/js/framework7.js
@@ -2,9 +2,9 @@ import t7 from 'template7';
 import $ from 'dom7';
 
 // F7 Class
-import Framework7 from './classes/framework7';
+import Framework7 from './classes/framework7-class';
 
-// Import Modules
+// Import Core Modules
 import Resize from './modules/resize';
 import Device from './modules/device';
 import Support from './modules/support';
@@ -13,7 +13,13 @@ import Touch from './modules/touch';
 import Router from './modules/router';
 import View from './modules/view';
 
-// import Demo from './demo-module';
+// Additional Modules
+import Sortable from './modules/sortable';
+import Swipeout from './modules/swipeout';
+import SmartSelect from './modules/smartselect';
+import Calendar from './modules/calendar';
+import Picker from './modules/picker';
+import Panels from './modules/panels';
 
 // Template7
 Framework7.prototype.t7 = t7;
@@ -23,12 +29,20 @@ Framework7.prototype.$ = $;
 
 // Install Modules
 Framework7
+  // Core Modules
   .use(Events)
   .use(Support)
   .use(Device)
   .use(Resize)
   .use(Touch)
   .use(Router)
-  .use(View);
+  .use(View)
+  // Additional Modules
+  .use(Sortable)
+  .use(Swipeout)
+  .use(SmartSelect)
+  .use(Calendar)
+  .use(Picker)
+  .use(Panels);
 
 export default Framework7;
