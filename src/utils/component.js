2bb7f0271832eeefeac56a9f6141a866f03d54ce
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-05-19 13:36:28 +0300

    Better component loader

--- src/utils/component.js
+++ src/utils/component.js
@@ -3,7 +3,33 @@ import t7 from 'template7';
 import Utils from '../utils/utils';
 
 const Component = {
-  render(c, extend = {}) {
+  parse(componentString) {
+    const callbackName = `f7_component_callback_${new Date().getTime()}`;
+
+    let template;
+    if (componentString.indexOf('<template>') >= 0) {
+      template = componentString.split('<template>')[1].split('</template>')[0].trim();
+    }
+
+    let scriptContent = componentString.split('<script>')[1].split('</script>')[0].trim();
+    scriptContent = `window.${callbackName} = function () {${scriptContent}}`;
+
+    // Insert Script El
+    const scriptEl = document.createElement('script');
+    scriptEl.innerHTML = scriptContent;
+    $('head').append(scriptEl);
+
+    const component = window[callbackName]();
+
+    // Remove Script El
+    $(scriptEl).remove();
+
+    if (!component.template && !component.render) {
+      component.template = template;
+    }
+    return component;
+  },
+  compile(c, extend = {}) {
     const component = c;
     const context = Utils.extend(component, extend);
 
@@ -41,46 +67,6 @@ const Component = {
       html,
       component,
     };
-  },
-  get(url, callback) {
-    return Utils.promise((resolve, reject) => {
-      $.ajax({
-        url,
-        method: 'GET',
-        success(res) {
-          const callbackName = `f7_component_callback_${new Date().getTime()}`;
-
-          let template;
-          if (res.indexOf('<template>') >= 0) {
-            template = res.split('<template>')[1].split('</template>')[0].trim();
-          }
-
-          let scriptContent = res.split('<script>')[1].split('</script>')[0].trim();
-          scriptContent = `window.${callbackName} = function () {${scriptContent}}`;
-
-
-          // Insert Script El
-          const scriptEl = document.createElement('script');
-          scriptEl.innerHTML = scriptContent;
-          $('head').append(scriptEl);
-
-          const component = window[callbackName]();
-
-          // Remove Script El
-          $(scriptEl).remove();
-
-          if (!component.template && !component.render) {
-            component.template = template;
-          }
-
-          if (callback) callback(component);
-          resolve(component);
-        },
-        error(xhr) {
-          reject(xhr);
-        },
-      });
-    });
   },
 };
 export default Component;
