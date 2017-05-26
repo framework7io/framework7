f49da57fedb1224e7aa28e037efc55455c43f163
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-05-26 17:33:29 +0300

    Finalised Dialog

--- src/components/dialog/dialog-class.js
+++ src/components/dialog/dialog-class.js
@@ -15,36 +15,50 @@ class Dialog extends Modal {
     }, params);
 
     // Extends with open/close Modal methods;
-    super(app);
+    super(app, extendedParams);
 
     const dialog = this;
 
-    const { title, text, contentAfterText, buttons, verticalButtons } = extendedParams;
+    const { title, text, contentAfterText, buttons, verticalButtons, cssClass } = extendedParams;
 
-    const dialogClasses = ['dialog'];
-    if (buttons.length === 0) dialogClasses.push('dialog-no-buttons');
-    if (buttons.length > 0) dialogClasses.push(`dialog-buttons-${buttons.length}`);
-    if (verticalButtons) dialogClasses.push('dialog-buttons-vertical');
+    dialog.params = extendedParams;
 
-    let buttonsHTML = '';
-    if (buttons.length > 0) {
-      buttonsHTML = `
-        <div class="dialog-buttons">
-          ${buttons.map(button => `<span class="dialog-button${button.bold ? ' dialog-button-bold' : ''}">${button.text}</span>`)}
+    // Find Element
+    let $el;
+    if (!dialog.params.el) {
+      const dialogClasses = ['dialog'];
+      if (buttons.length === 0) dialogClasses.push('dialog-no-buttons');
+      if (buttons.length > 0) dialogClasses.push(`dialog-buttons-${buttons.length}`);
+      if (verticalButtons) dialogClasses.push('dialog-buttons-vertical');
+      if (cssClass) dialogClasses.push(cssClass);
+
+      let buttonsHTML = '';
+      if (buttons.length > 0) {
+        buttonsHTML = `
+          <div class="dialog-buttons">
+            ${buttons.map(button => `<span class="dialog-button${button.bold ? ' dialog-button-bold' : ''}">${button.text}</span>`).join('')}
+          </div>
+        `;
+      }
+
+      const dialogHtml = `
+        <div class="${dialogClasses.join(' ')}">
+          <div class="dialog-inner">
+            ${title ? `<div class="dialog-title">${title}</div>` : ''}
+            ${text ? `<div class="dialog-text">${text}</div>` : ''}
+            ${contentAfterText}
+          </div>
+          ${buttonsHTML}
         </div>
       `;
+      $el = $(dialogHtml);
+    } else {
+      $el = $(dialog.params.el);
     }
 
-    const dialogHtml = `
-      <div class="${dialogClasses.join(' ')}">
-        <div class="dialog-inner">
-          ${title ? `<div class="dialog-title">${title}</div>` : ''}
-          ${text ? `<div class="dialog-text">${text}</div>` : ''}
-          ${contentAfterText}
-        </div>
-        ${buttonsHTML}
-      </div>
-    `;
+    if ($el && $el.length > 0 && $el[0].f7Modal) {
+      return $el[0].f7Modal;
+    }
 
     let $overlayEl = app.root.children('.dialog-overlay');
     if ($overlayEl.length === 0) {
@@ -52,57 +66,37 @@ class Dialog extends Modal {
       app.root.append($overlayEl);
     }
 
+    // Assign events
+    function buttonOnClick(e) {
+      const buttonEl = this;
+      const index = $(buttonEl).index();
+      const button = buttons[index];
+      if (button.onClick) button.onClick(dialog, e);
+      if (dialog.params.onClick) dialog.params.onClick(dialog, index);
+      if (button.close !== false) dialog.close();
+    }
+    if (buttons && buttons.length > 0) {
+      $el.find('.dialog-button').each((index, buttonEl) => {
+        $(buttonEl).on('click', buttonOnClick);
+      });
+      dialog.on('close', () => {
+        $el.find('.dialog-button').each((index, buttonEl) => {
+          $(buttonEl).off('click', buttonOnClick);
+        });
+      });
+    }
     Utils.extend(dialog, {
       app,
-      html: dialogHtml,
-      params: extendedParams,
+      $el,
+      el: $el[0],
       $overlayEl,
       overlayEl: $overlayEl[0],
       type: 'dialog',
     });
 
+    $el[0].f7Modal = dialog;
+
     return dialog;
-    // if (app.params.modalTemplate) {
-    //     if (!app._compiledTemplates.modal) app._compiledTemplates.modal = t7.compile(app.params.modalTemplate);
-    //     modalHTML = app._compiledTemplates.modal(params);
-    // }
-    // else {
-    //     var buttonsHTML = '';
-    //     if (params.buttons && params.buttons.length > 0) {
-    //         for (var i = 0; i < params.buttons.length; i++) {
-    //             buttonsHTML += '<span class="modal-button' + (params.buttons[i].bold ? ' modal-button-bold' : '') + '">' + params.buttons[i].text + '</span>';
-    //         }
-    //     }
-    //     var titleHTML = params.title ? '<div class="modal-title">' + params.title + '</div>' : '';
-    //     var textHTML = params.text ? '<div class="modal-text">' + params.text + '</div>' : '';
-    //     var afterTextHTML = params.afterText ? params.afterText : '';
-    //     var noButtons = !params.buttons || params.buttons.length === 0 ? 'modal-no-buttons' : '';
-    //     var verticalButtons = params.verticalButtons ? 'modal-buttons-vertical': '';
-    //     var modalButtonsHTML = params.buttons && params.buttons.length > 0 ? '<div class="modal-buttons modal-buttons-' + params.buttons.length + ' ' + verticalButtons + '">' + buttonsHTML + '</div>' : '';
-    //     modalHTML = '<div class="modal ' + noButtons + ' ' + (params.cssClass || '') + '"><div class="modal-inner">' + (titleHTML + textHTML + afterTextHTML) + '</div>' + modalButtonsHTML + '</div>';
-    // }
-
-    // _modalTemplateTempDiv.innerHTML = modalHTML;
-
-    // var modal = $(_modalTemplateTempDiv).children();
-
-    // app.root.append(modal[0]);
-
-    // // Add events on buttons
-    // modal.find('.modal-button').each(function (index, el) {
-    //     $(el).on('click', function (e) {
-    //         if (params.buttons[index].close !== false) app.closeModal(modal);
-    //         if (params.buttons[index].onClick) params.buttons[index].onClick(modal, e);
-    //         if (params.onClick) params.onClick(modal, index);
-    //     });
-    // });
-    // app.openModal(modal);
-
-    // Utils.extend(dialog, {
-    //   params: extendedParams,
-    // });
-
-    // return dialog;
   }
 }
 export default Dialog;
--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -1,3 +1,4 @@
+import $ from 'dom7';
 import Utils from '../../utils/utils';
 import Dialog from './dialog-class';
 
@@ -10,14 +11,26 @@ export default {
         create(params) {
           return new Dialog(app, params);
         },
-        open() {},
-        close() {},
+        open(dialogEl, animate) {
+          return new Dialog(app, {
+            el: $(dialogEl),
+          }).open(animate);
+        },
+        close(dialogEl = '.dialog.modal-in', animate) {
+          return new Dialog(app, {
+            el: $(dialogEl),
+          }).close(animate);
+        },
 
         // Shortcuts
-        alert(text, title, callbackOk) {
+        alert(...args) {
+          let [text, title, callbackOk] = args;
+          if (args.length === 2 && typeof args[1] === 'function') {
+            [text, callbackOk, title] = args;
+          }
           return new Dialog(app, {
-            text: text || '',
             title: typeof title === 'undefined' ? app.params.modals.dialogTitle : title,
+            text,
             buttons: [{
               text: app.params.modals.dialogButtonOk,
               bold: true,
@@ -25,13 +38,120 @@ export default {
             }],
           }).open();
         },
-        prompt() {},
-        confirm() {},
-        login() {},
-        password() {},
-        preloader() {},
-        indicator() {},
-        progress() {},
+        prompt(...args) {
+          let [text, title, callbackOk, callbackCancel] = args;
+          if (typeof args[1] === 'function') {
+            [text, callbackOk, callbackCancel, title] = args;
+          }
+          return new Dialog(app, {
+            title: typeof title === 'undefined' ? app.params.modals.dialogTitle : title,
+            text,
+            contentAfterText: '<div class="dialog-input-field"><input type="text" class="dialog-input"></div>',
+            buttons: [
+              {
+                text: app.params.modals.dialogButtonCancel,
+              },
+              {
+                text: app.params.modals.dialogButtonOk,
+                bold: true,
+              },
+            ],
+            onClick(dialog, index) {
+              const inputValue = dialog.$el.find('.dialog-input').val();
+              if (index === 0 && callbackCancel) callbackCancel(inputValue);
+              if (index === 1 && callbackOk) callbackOk(inputValue);
+            },
+          }).open();
+        },
+        confirm(...args) {
+          let [text, title, callbackOk, callbackCancel] = args;
+          if (typeof args[1] === 'function') {
+            [text, callbackOk, callbackCancel, title] = args;
+          }
+          return new Dialog(app, {
+            title: typeof title === 'undefined' ? app.params.modals.dialogTitle : title,
+            text,
+            buttons: [
+              {
+                text: app.params.modals.dialogButtonCancel,
+                onClick: callbackCancel,
+              },
+              {
+                text: app.params.modals.dialogButtonOk,
+                bold: true,
+                onClick: callbackOk,
+              },
+            ],
+          }).open();
+        },
+        login(...args) {
+          let [text, title, callbackOk, callbackCancel] = args;
+          if (typeof args[1] === 'function') {
+            [text, callbackOk, callbackCancel, title] = args;
+          }
+          return new Dialog(app, {
+            title: typeof title === 'undefined' ? app.params.modals.dialogTitle : title,
+            text,
+            contentAfterText: `
+              <div class="dialog-input-field dialog-input-double">
+                <input type="text" name="dialog-username" placeholder="${app.params.modals.dialogUsernamePlaceholder}" class="dialog-input">
+              </div>
+              <div class="dialog-input-field dialog-input-double">
+                <input type="password" name="dialog-password" placeholder="${app.params.modals.dialogPasswordPlaceholder}" class="dialog-input">
+              </div>`,
+            buttons: [
+              {
+                text: app.params.modals.dialogButtonCancel,
+              },
+              {
+                text: app.params.modals.dialogButtonOk,
+                bold: true,
+              },
+            ],
+            onClick(dialog, index) {
+              const username = dialog.$el.find('[name="dialog-username"]').val();
+              const password = dialog.$el.find('[name="dialog-password"]').val();
+              if (index === 0 && callbackCancel) callbackCancel(username, password);
+              if (index === 1 && callbackOk) callbackOk(username, password);
+            },
+          }).open();
+        },
+        password(...args) {
+          let [text, title, callbackOk, callbackCancel] = args;
+          if (typeof args[1] === 'function') {
+            [text, callbackOk, callbackCancel, title] = args;
+          }
+          return new Dialog(app, {
+            title: typeof title === 'undefined' ? app.params.modals.dialogTitle : title,
+            text,
+            contentAfterText: `
+              <div class="dialog-input-field">
+                <input type="password" name="dialog-password" placeholder="${app.params.modals.dialogPasswordPlaceholder}" class="dialog-input">
+              </div>`,
+            buttons: [
+              {
+                text: app.params.modals.dialogButtonCancel,
+              },
+              {
+                text: app.params.modals.dialogButtonOk,
+                bold: true,
+              },
+            ],
+            onClick(dialog, index) {
+              const password = dialog.$el.find('[name="dialog-password"]').val();
+              if (index === 0 && callbackCancel) callbackCancel(password);
+              if (index === 1 && callbackOk) callbackOk(password);
+            },
+          }).open();
+        },
+        preloader(title) {
+          return new Dialog(app, {
+            title: typeof title === 'undefined' ? app.params.modals.dialogTitle : title,
+            text: '<div class="preloader"></div>',
+            cssClass: 'dialog-preloader',
+          }).open();
+        },
+        progress(...args) {},
       },
     });
   },
