ffacc6684f6facea0ad37d7093d4d314528b6920
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-05-31 14:57:09 +0300

    Events parser

--- src/utils/component.js
+++ src/utils/component.js
@@ -2,6 +2,7 @@ import $ from 'dom7';
 import t7 from 'template7';
 import Utils from '../utils/utils';
 
+const tempDom = document.createElement('div');
 const Component = {
   parse(componentString) {
     const callbackName = `f7_component_callback_${new Date().getTime()}`;
@@ -31,7 +32,7 @@ const Component = {
   },
   compile(c, extend = {}) {
     const component = c;
-    const context = Utils.extend(component, extend);
+    const context = Utils.extend({}, extend);
 
     // Apply context
     if (component.data) {
@@ -42,7 +43,7 @@ const Component = {
     if (component.render) component.render = component.render.bind(context);
     if (component.methods) {
       Object.keys(component.methods).forEach((methodName) => {
-        component[methodName] = component.methods[methodName].bind(context);
+        context[methodName] = component.methods[methodName].bind(context);
       });
     }
     if (component.on) {
@@ -51,7 +52,7 @@ const Component = {
       });
     }
 
-    // Render
+    // Render template
     let html = '';
     if (component.render) {
       html = component.render();
@@ -63,9 +64,83 @@ const Component = {
         html = component.template(context);
       }
     }
+
+    // Make Dom
+    tempDom.innerHTML = html;
+
+    // Find Events
+    const events = [];
+    $(tempDom).find('*').each((index, el) => {
+      for (let i = 0; i < el.attributes.length; i += 1) {
+        const attr = el.attributes[i];
+        if (attr.name.indexOf('@') === 0) {
+          const event = attr.name.replace('@', '');
+          let name = event;
+          let stop = false;
+          let prevent = false;
+          let once = false;
+          if (event.indexOf('.') >= 0) {
+            event.split('.').forEach((eventNamePart, eventNameIndex) => {
+              if (eventNameIndex === 0) name = eventNamePart;
+              else {
+                if (eventNamePart === 'stop') stop = true;
+                if (eventNamePart === 'prevent') prevent = true;
+                if (eventNamePart === 'once') once = true;
+              }
+            });
+          }
+
+          const value = attr.value;
+          el.removeAttribute(attr.name);
+          events.push({
+            el,
+            name,
+            once,
+            handler: (e) => {
+              if (stop) e.stopPropagation();
+              if (prevent) e.preventDefault();
+              let methodName;
+              let method;
+              const args = [];
+              if (value.indexOf('(') < 0) {
+                args.push(e);
+                methodName = value;
+              } else {
+                methodName = value.split('(')[0];
+                value.split('(')[1].split(')')[0].split(',').forEach((argument) => {
+                  let arg = argument.trim();
+                  if (!isNaN(arg)) arg = parseFloat(arg);
+                  else if (arg[0] === '"') arg = arg.replace(/"/g, '');
+                  else if (arg[0] === '\'') arg = arg.replace(/'/g, '');
+                  args.push(arg);
+                });
+              }
+              if (methodName.indexOf('.') >= 0) {
+                methodName.split('.').forEach((path, pathIndex) => {
+                  if (!method) method = context;
+                  if (method[path]) method = method[path];
+                  else {
+                    throw new Error(`Component doesn't have method "${methodName.split('.').slice(0, pathIndex + 1).join('.')}"`);
+                  }
+                });
+              } else {
+                if (!context[methodName]) {
+                  throw new Error(`Component doesn't have method "${methodName}"`);
+                }
+                method = context[methodName];
+              }
+              method(...args);
+            },
+          });
+        }
+      }
+    });
+
     return {
+      dom: tempDom.children,
+      events,
       html,
-      component,
+      context,
     };
   },
 };
