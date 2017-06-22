cfb70850d827bda24f812e884bbdfe22c3b7e922
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-06-22 14:47:20 +0300

    Remove comments

--- src/modules/router/router.js
+++ src/modules/router/router.js
@@ -2,45 +2,6 @@ import Router from './router-class';
 
 export default {
   name: 'router',
-  params: {
-    router: {
-      // stackPages: false,
-      // xhrCache: true,
-      // xhrCacheIgnore: [],
-      // xhrCacheIgnoreGetParameters: false,
-      // xhrCacheDuration: 1000 * 60 * 10, // Ten minutes
-      // preloadPreviousPage: true,
-      // uniqueHistory: false,
-      // uniqueHistoryIgnoreGetParameters: false,
-      // allowDuplicateUrls: false,
-      // reloadPages: false,
-      // removeElements: true,
-      // removeElementsWithTimeout: false,
-      // removeElementsTimeout: 0,
-      // // Swipe Back
-      // swipeBackPage: true,
-      // swipeBackPageAnimateShadow: true,
-      // swipeBackPageAnimateOpacity: true,
-      // swipeBackPageActiveArea: 30,
-      // swipeBackPageThreshold: 0,
-      // // Push State
-      // pushState: false,
-      // pushStateRoot: undefined,
-      // pushStateAnimate: true,
-      // pushStateAnimateOnLoad: false,
-      // pushStateSeparator: '#!',
-      // pushStateOnLoad: true,
-      // // Animate Pages
-      // animate: true,
-      // // iOS Dynamic Navbar
-      // iosDynamicNavbar: true,
-      // iosSeparateDynamicNavbar: true,
-      // // Animate iOS Navbar Back Icon
-      // iosAnimateNavbarBackIcon: true,
-      // // MD Theme delay
-      // materialPageLoadDelay: 0,
-    },
-  },
   static: {
     Router,
   },
