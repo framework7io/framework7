0ab82d7453dd772ed034640735760a371a5d57a7
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-06-30 17:13:18 +0300

    Check for correct position sticky support

--- src/modules/support/support.js
+++ src/modules/support/support.js
@@ -8,4 +8,20 @@ export default {
   static: {
     Support,
   },
+  on: {
+    init() {
+      const html = document.querySelector('html');
+      const classNames = [];
+      if (Support.positionSticky) {
+        classNames.push('support-position-sticky');
+        if (Support.positionStickyFalsy) {
+          classNames.push('support-position-sticky-falsy');
+        }
+      }
+      // Add html classes
+      classNames.forEach((className) => {
+        html.classList.add(className);
+      });
+    },
+  },
 };
--- src/utils/support.js
+++ src/utils/support.js
@@ -16,10 +16,38 @@ function supportTouch() {
   return !!(('ontouchstart' in window) || (window.DocumentTouch && document instanceof window.DocumentTouch));
 }
 
+let positionSticky = false;
+function supportPositionSticky() {
+  const div = document.createElement('div');
+  ('sticky -webkit-sticky -moz-sticky').split(' ').forEach((prop) => {
+    if (positionSticky) return;
+    div.style.position = prop;
+    if (div.style.position === prop) positionSticky = prop;
+  });
+}
+supportPositionSticky();
+function positionStickyFalsy() {
+  let falsy = false;
+  if (!positionSticky) return falsy;
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div id="position-sticky-test" style="overflow:scroll; height: 100px; width:100px; position: absolute; left:0px; top:0px; padding-top:50px; visibility: hidden;">
+      <div id="position-sticky-test-element" style="margin:0; padding:0; height:10px; width:100%; position:${positionSticky}; top:0"></div>
+      <div style="height: 1000px"></div>
+    </div>`;
+  document.body.appendChild(div);
+  document.getElementById('position-sticky-test').scrollTop = 50;
+  if (document.getElementById('position-sticky-test-element').offsetTop === 50) {
+    falsy = true;
+  }
+  return falsy;
+}
 const Support = {
   touch: supportTouch(),
   // Passive Listeners
   passiveListener: supportsPassiveListener(),
+  positionSticky,
+  positionStickyFalsy: positionStickyFalsy(),
 };
 
 export default Support;
