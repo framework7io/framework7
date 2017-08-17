e4e1e23b743e33ffbc5f3c746a7bfe9b83468422
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-17 17:59:21 +0300

    Finalize panel demo

--- kitchen-sink/js/routes.js
+++ kitchen-sink/js/routes.js
@@ -1,34 +1,62 @@
 var routes = [
-  // Load page by URL
+  // Index page
   {
     path: '/',
     url: 'index.html',
   },
-  // Load page by URL
+  // About page
   {
     path: '/about/',
     url: './about.html',
   },
+  // Right Panel pages
   {
-    path: '/news/',
-    url: './news.html',
+    path: '/panel-right-1/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 1</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 1</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
   },
   {
-    path: '/services/',
-    url: './services.html',
-  },
-  {
-    path: '/component/',
-    componentUrl: './component.html',
-  },
-  {
-    path: '/template/',
-    templateUrl: './template.html',
-    options: {
-      context: {
-        title: 'Template test',
-      },
-    },
+    path: '/panel-right-2/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 2</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 2</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
   },
 
   // Toolbars
@@ -48,14 +76,7 @@ var routes = [
     path: '/tabbar-scrollable/',
     url: './tabbar-scrollable.html',
   },
-  {
-    path: '/login-screen/',
-    componentUrl: './login-screen.html',
-  },
-  {
-    path: '/login-screen-page/',
-    componentUrl: './login-screen-page.html',
-  },
+
 
   // Components
   {
@@ -129,6 +150,14 @@ var routes = [
   {
     path: '/list/',
     url: './list.html',
+  },
+  {
+    path: '/login-screen/',
+    componentUrl: './login-screen.html',
+  },
+  {
+    path: '/login-screen-page/',
+    componentUrl: './login-screen-page.html',
   },
   {
     path: '/messages/',
