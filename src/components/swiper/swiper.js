d317e3557fab828e38df7be26806b78b28aebe4b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-08-30 23:36:01 +0300

    Introduce Swiper component

---
+++ src/components/swiper/swiper.js
@@ -0,0 +1,12 @@
+import Swiper from 'swiper';
+import $ from '../../utils/dom7';
+
+export default {
+  name: 'swiper',
+  static: {
+    Swiper,
+  },
+  create() {
+    const app = this;
+  },
+};
