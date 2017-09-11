1b3869f8f6eea14297b29caa474972c4ec5848ee
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-11 19:39:46 +0300

    Swiper uses F7's Support

--- src/utils/support.js
+++ src/utils/support.js
@@ -1,54 +1,80 @@
-function supportsPassiveListener() {
-  let supportsPassive = false;
-  try {
-    const opts = Object.defineProperty({}, 'passive', {
-      get() {
-        supportsPassive = true;
-      },
+function Support() {
+  let positionStickyProp;
+  const positionSticky = (function supportPositionSticky() {
+    let support = false;
+    const div = document.createElement('div');
+    ('sticky -webkit-sticky -moz-sticky').split(' ').forEach((prop) => {
+      if (support) return;
+      div.style.position = prop;
+      if (div.style.position === prop) {
+        support = true;
+        positionStickyProp = prop;
+      }
     });
-    window.addEventListener('testPassiveListener', null, opts);
-  } catch (e) {
-    supportsPassive = false;
-  }
-  return supportsPassive;
-}
-function supportTouch() {
-  return !!(('ontouchstart' in window) || (window.DocumentTouch && document instanceof window.DocumentTouch));
-}
+    return support;
+  }());
 
-let positionSticky = false;
-function supportPositionSticky() {
-  const div = document.createElement('div');
-  ('sticky -webkit-sticky -moz-sticky').split(' ').forEach((prop) => {
-    if (positionSticky) return;
-    div.style.position = prop;
-    if (div.style.position === prop) positionSticky = prop;
-  });
-}
-supportPositionSticky();
-function positionStickyFalsy() {
-  let falsy = false;
-  if (!positionSticky) return falsy;
-  const div = document.createElement('div');
-  div.innerHTML = `
-    <div id="position-sticky-test" style="overflow:scroll; height: 100px; width:100px; position: absolute; left:0px; top:0px; padding-top:50px; visibility: hidden;">
-      <div id="position-sticky-test-element" style="margin:0; padding:0; height:10px; width:100%; position:${positionSticky}; top:0"></div>
-      <div style="height: 1000px"></div>
-    </div>`;
-  document.body.appendChild(div);
-  document.getElementById('position-sticky-test').scrollTop = 50;
-  if (document.getElementById('position-sticky-test-element').offsetTop === 50) {
-    falsy = true;
-  }
-  div.parentNode.removeChild(div);
-  return falsy;
-}
-const Support = {
-  touch: supportTouch(),
-  // Passive Listeners
-  passiveListener: supportsPassiveListener(),
-  positionSticky,
-  positionStickyFalsy: positionStickyFalsy(),
-};
+  const positionStickyFalsy = (function positionStickyFalsy() {
+    let falsy = false;
+    if (!positionStickyProp) return false;
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div id="position-sticky-test" style="overflow:scroll; height: 100px; width:100px; position: absolute; left:0px; top:0px; padding-top:50px; visibility: hidden;">
+        <div id="position-sticky-test-element" style="margin:0; padding:0; height:10px; width:100%; position:${positionStickyProp}; top:0"></div>
+        <div style="height: 1000px"></div>
+      </div>`;
+    document.body.appendChild(div);
+    document.getElementById('position-sticky-test').scrollTop = 50;
+    if (document.getElementById('position-sticky-test-element').offsetTop === 50) {
+      falsy = true;
+    }
+    div.parentNode.removeChild(div);
+    return falsy;
+  }());
 
-export default Support;
+  return {
+    positionSticky,
+    positionStickyFalsy,
+    touch: (function checkTouch() {
+      return !!(('ontouchstart' in window) || (window.DocumentTouch && document instanceof window.DocumentTouch));
+    }()),
+
+    transforms3d: (function checkTransforms3d() {
+      const div = document.createElement('div').style;
+      return ('webkitPerspective' in div || 'MozPerspective' in div || 'OPerspective' in div || 'MsPerspective' in div || 'perspective' in div);
+    }()),
+
+    flexbox: (function checkFlexbox() {
+      const div = document.createElement('div').style;
+      const styles = ('alignItems webkitAlignItems webkitBoxAlign msFlexAlign mozBoxAlign webkitFlexDirection msFlexDirection mozBoxDirection mozBoxOrient webkitBoxDirection webkitBoxOrient').split(' ');
+      for (let i = 0; i < styles.length; i += 1) {
+        if (styles[i] in div) return true;
+      }
+      return false;
+    }()),
+
+    observer: (function checkObserver() {
+      return ('MutationObserver' in window || 'WebkitMutationObserver' in window);
+    }()),
+
+    passiveListener: (function checkPassiveListener() {
+      let supportsPassive = false;
+      try {
+        const opts = Object.defineProperty({}, 'passive', {
+          get() {
+            supportsPassive = true;
+          },
+        });
+        window.addEventListener('testPassiveListener', null, opts);
+      } catch (e) {
+        // No support
+      }
+      return supportsPassive;
+    }()),
+
+    gestures: (function checkGestures() {
+      return 'ongesturestart' in window;
+    }()),
+  };
+}
+export default Support();
