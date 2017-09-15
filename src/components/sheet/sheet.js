2206c8cdade99447bbbd22bd61dc31c96e405161
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-15 19:09:17 +0300

    Close other sheet modals

--- src/components/sheet/sheet.js
+++ src/components/sheet/sheet.js
@@ -1,3 +1,4 @@
+import $ from 'dom7';
 import Utils from '../../utils/utils';
 import Sheet from './sheet-class';
 import ModalMethods from '../../utils/modal-methods';
@@ -20,6 +21,9 @@ export default {
   clicks: {
     '.sheet-open': function openSheet($clickedEl, data = {}) {
       const app = this;
+      if ($('.sheet-modal.modal-in').length > 0 && data.sheet && $(data.sheet)[0] !== $('.sheet-modal.modal-in')[0]) {
+        app.sheet.close('.sheet-modal.modal-in');
+      }
       app.sheet.open(data.sheet, data.animate);
     },
     '.sheet-close': function closeSheet($clickedEl, data = {}) {
