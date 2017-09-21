a4d9da16f687cc38793a588caddcddc5acdbf3d5
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-21 16:13:56 +0300

    Move specific modals params under their own sections

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -8,27 +8,7 @@ export default {
   params: {
     modals: {
       moveToRoot: true,
-
       queueDialogs: true,
-      dialogTitle: 'Framework7',
-      dialogButtonOk: 'OK',
-      dialogButtonCancel: 'Cancel',
-      dialogUsernamePlaceholder: 'Username',
-      dialogPasswordPlaceholder: 'Password',
-      dialogPreloaderTitle: 'Loading... ',
-      dialogProgressTitle: 'Loading... ',
-      dialogCloseByBackdropClick: false,
-
-      popupCloseByBackdropClick: true,
-
-      popoverCloseByBackdropClick: true,
-      popoverCloseByOutsideClick: false,
-
-      actionsToPopover: true,
-      actionsCloseByBackdropClick: true,
-
-      sheetCloseByBackdropClick: true,
-      sheetCloseByOutsideClick: false,
     },
   },
 };
