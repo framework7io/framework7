9c2a5d697d39832dcdff866faff3ae0fefb0d2ba
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-10-23 22:08:37 +0300

    Dynamic config file

---
+++ scripts/get-config.js
@@ -0,0 +1,31 @@
+/* eslint import/no-unresolved: "off" */
+/* eslint global-require: "off" */
+
+const path = require('path');
+let config = require('./build-config.js');
+
+// Overwrite with local config
+try {
+  const customConfig = require('./build-config-custom.js');
+  config = Object.assign({}, config, customConfig);
+} catch (err) {
+  // No local config
+}
+
+let logged = false;
+
+function getConfig() {
+  const args = process.argv;
+  if (args[3] && args[3].indexOf('--config') === 0 && args[4]) {
+    const configPath = path.resolve(args[4]);
+    const overwriteConfig = require(configPath); // eslint-disable-line
+    config = Object.assign({}, config, overwriteConfig);
+    if (!logged) {
+      console.log(`Building using custom config from ${configPath}`);
+    }
+    logged = true;
+  }
+  return config;
+}
+
+module.exports = getConfig;
