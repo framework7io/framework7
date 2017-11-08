b9f8ee3b57e2019457bc0ca2573587ccf2d08788
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-11-08 14:44:07 +0300

    Remove diff task

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -2,31 +2,10 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect');
 const gopen = require('gulp-open');
-const fs = require('fs');
-const diff = require('diff');
 
 const buildKs = require('./build-ks.js');
 const buildJs = require('./build-js.js');
 const buildLess = require('./build-less.js');
-
-gulp.task('diff', () => {
-  const file = process.argv.slice(3).toString().replace('-', '');
-  const ios = fs.readFileSync(`./src/_less-old/ios/${file}.less`, 'utf8');
-  const md = fs.readFileSync(`./src/_less-old/material/${file}.less`, 'utf8');
-  const result = diff.diffLines(ios, md);
-
-  result.forEach((part) => {
-    // green for additions, red for deletions
-    // grey for common parts
-    let color = '\x1b[37m%s\x1b[0m';
-    if (part.added) {
-      color = '\x1b[31m%s\x1b[0m';
-    } else if (part.removed) {
-      color = '\x1b[32m%s\x1b[0m';
-    }
-    console.log(color, part.value);
-  });
-});
 
 // Tasks
 gulp.task('ks', (cb) => {
