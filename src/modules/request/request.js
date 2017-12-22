e6b09b504ae854c1d4e5ef88248c2f894d6d796f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-12-22 10:07:05 +0300

    add as proto

--- src/modules/request/request.js
+++ src/modules/request/request.js
@@ -3,9 +3,8 @@ import Request from '../../utils/request';
 
 export default {
   name: 'request',
-  create() {
-    const app = this;
-    app.request = Request;
+  proto: {
+    request: Request,
   },
   static: {
     request: Request,
