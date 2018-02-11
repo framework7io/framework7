7696f634c33a09c2cc35847c8d058f952fdd0b57
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-02-11 14:00:51 +0300

    Additional context demo

--- kitchen-sink/js/routes.js
+++ kitchen-sink/js/routes.js
@@ -455,10 +455,22 @@ var routes = [
   {
     path: '/page-loader-template7/:user/:userId/:posts/:postId/',
     templateUrl: './pages/page-loader-template7.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
   },
   {
     path: '/page-loader-component/:user/:userId/:posts/:postId/',
     componentUrl: './pages/page-loader-component.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
   },
 
   // Default route (404 page). MUST BE THE LAST
