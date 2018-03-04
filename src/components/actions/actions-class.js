2e1cfede5b1ddb18d878cc6e1f54e1e415c72ca9
zhennann <zhen.nann@icloud.com>
2018-03-04 18:37:01 +0800

    fix: add closeByOutsideClick, not handle button click if action.groups not specified in framework7-vue

--- src/components/actions/actions-class.js
+++ src/components/actions/actions-class.js
@@ -102,14 +102,18 @@ class Actions extends Modal {
         });
         popover.open(animate);
         popover.once('popoverOpened', () => {
-          popover.$el.find('.item-link').each((groupIndex, buttonEl) => {
-            $(buttonEl).on('click', buttonOnClick);
-          });
+          if (actions.groups) {
+            popover.$el.find('.item-link').each((groupIndex, buttonEl) => {
+              $(buttonEl).on('click', buttonOnClick);
+            });
+          }
         });
         popover.once('popoverClosed', () => {
-          popover.$el.find('.item-link').each((groupIndex, buttonEl) => {
-            $(buttonEl).off('click', buttonOnClick);
-          });
+          if (actions.groups) {
+            popover.$el.find('.item-link').each((groupIndex, buttonEl) => {
+              $(buttonEl).off('click', buttonOnClick);
+            });
+          }
           Utils.nextTick(() => {
             popover.destroy();
             popover = undefined;
@@ -118,14 +122,16 @@ class Actions extends Modal {
       } else {
         actions.$el = actions.actionsHtml ? $(actions.actionsHtml) : actions.$el;
         actions.$el[0].f7Modal = actions;
-        actions.$el.find('.actions-button').each((groupIndex, buttonEl) => {
-          $(buttonEl).on('click', buttonOnClick);
-        });
-        actions.once('actionsClosed', () => {
-          actions.$el.find('.list-button').each((groupIndex, buttonEl) => {
-            $(buttonEl).off('click', buttonOnClick);
+        if (actions.groups) {
+          actions.$el.find('.actions-button').each((groupIndex, buttonEl) => {
+            $(buttonEl).on('click', buttonOnClick);
           });
-        });
+          actions.once('actionsClosed', () => {
+            actions.$el.find('.list-button').each((groupIndex, buttonEl) => {
+              $(buttonEl).off('click', buttonOnClick);
+            });
+          });
+        }
         originalOpen.call(actions, animate);
       }
       return actions;
@@ -160,17 +166,19 @@ class Actions extends Modal {
           actions.backdropEl === target
         ) {
           actions.close();
+        } else if (actions.params.closeByOutsideClick) {
+          actions.close();
         }
       }
     }
 
     actions.on('opened', () => {
-      if (actions.params.closeByBackdropClick) {
+      if (actions.params.closeByBackdropClick || actions.params.closeByOutsideClick) {
         app.on('click', handleClick);
       }
     });
     actions.on('close', () => {
-      if (actions.params.closeByBackdropClick) {
+      if (actions.params.closeByBackdropClick || actions.params.closeByOutsideClick) {
         app.off('click', handleClick);
       }
     });
