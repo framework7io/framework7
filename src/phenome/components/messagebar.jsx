630947dc69ab676def0becaae1094df57206f39c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-05-24 19:33:36 +0300

    Phenome messagebar: remove undefined props

--- src/phenome/components/messagebar.jsx
+++ src/phenome/components/messagebar.jsx
@@ -193,15 +193,21 @@ export default {
     el.addEventListener('messagebar:attachmentclick', self.onClickAttachmentBound);
     el.addEventListener('messagebar:resizepage', self.onResizePageBound);
 
+    const params = {
+      el,
+      top,
+      resizePage,
+      bottomOffset,
+      topOffset,
+      maxHeight,
+    };
+
+    Object.keys(params).forEach((key) => {
+      if (typeof params[key] === 'undefined') delete params[key];
+    });
+
     self.$f7ready(() => {
-      self.f7Messagebar = self.$f7.messagebar.create({
-        el,
-        top,
-        resizePage,
-        bottomOffset,
-        topOffset,
-        maxHeight,
-      });
+      self.f7Messagebar = self.$f7.messagebar.create(params);
     });
   },
   componentDidUpdate() {
