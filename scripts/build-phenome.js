87737bb1f3cb84c56e2240a04bd80ec1c8436bef
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-06-11 17:11:56 +0300

    Transform object spread in phenome components

--- scripts/build-phenome.js
+++ scripts/build-phenome.js
@@ -3,11 +3,35 @@
 /* eslint global-require: "off" */
 /* eslint no-param-reassign: ["error", { "props": false }] */
 const phenome = require('phenome');
+const fs = require('fs');
+const { transformSync } = require('@babel/core');
+
+function transformRestSpread(buildPath) {
+  const reactFiles = fs.readdirSync(`${buildPath}/react/components`).filter(f => f[0] !== '.');
+  const vueFiles = fs.readdirSync(`${buildPath}/vue/components`).filter(f => f[0] !== '.');
+
+  function transformFile(filePath) {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const { code } = transformSync(fileContent, {
+      plugins: [
+        ['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
+      ],
+    });
+    fs.writeFileSync(filePath, code);
+  }
+  reactFiles.forEach((fileName) => {
+    transformFile(`${buildPath}/react/components/${fileName}`);
+  });
+  vueFiles.forEach((fileName) => {
+    transformFile(`${buildPath}/vue/components/${fileName}`);
+  });
+}
 
 // Phenome
 function build(cb) {
   const env = process.env.NODE_ENV || 'development';
   const buildPath = env === 'development' ? './build' : './packages';
+
   phenome({
     paths: ['./src/phenome/**/*.js', './src/phenome/**/*.jsx'],
     react: {
@@ -39,6 +63,7 @@ function build(cb) {
       },
     },
   }).then(() => {
+    transformRestSpread(buildPath);
     if (cb) cb();
   }).catch((err) => {
     console.log(err);
