c59b5a3313fc1a827f0ae044bfbc93b80c4231ba
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-07-31 11:22:44 +0300

    Core: Searchbar - trigger events on VL clear
    Fixes #2528

--- src/core/components/searchbar/searchbar-class.js
+++ src/core/components/searchbar/searchbar-class.js
@@ -427,6 +427,8 @@ class Searchbar extends FrameworkClass {
         sb.virtualList.resetFilter();
         if ($notFoundEl) $notFoundEl.hide();
         if ($foundEl) $foundEl.show();
+        $el.trigger('searchbar:search', query, sb.previousQuery);
+        sb.emit('local::search searchbarSearch', sb, query, sb.previousQuery);
         return sb;
       }
       vlQuery = sb.params.removeDiacritics ? Utils.removeDiacritics(query) : query;
