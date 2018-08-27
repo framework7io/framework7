023c09428a64301caa55ee264dc80d11a311e965
Sepehr Raftari <raftari.dev@gmail.com>
2018-08-27 19:00:16 +0430

    bug fix

--- src/core/components/calendar/calendar.js
+++ src/core/components/calendar/calendar.js
@@ -76,7 +76,7 @@ export default {
       routableModals: true,
       view: null,
       url: 'date/',
-      backdrop: false,
+      backdrop: null,
       closeByBackdropClick: true,
       // Render functions
       renderWeekHeader: null,
