7fb7070afb0c6b6c8d2e6c349f43d93fea70af26
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-09-05 20:22:13 +0300

    DTS: Device module

---
+++ src/core/modules/device/device.d.ts
@@ -0,0 +1,19 @@
+import Device from '../../utils/device';
+import Framework7 from '../../components/app/app-class';
+
+namespace DeviceModule {
+
+}
+
+declare module '../../components/app/app-class' {
+  interface Framework7Class {
+    /** Object with properties about device */
+    device: Device
+  }
+  interface Framework7Params {
+  }
+  interface Framework7AppEvents {
+  }
+}
+
+export default DeviceModule;
