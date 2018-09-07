b7b91523eadf4fc9a7c5f3cbf475dc97bb484643
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-09-07 14:35:51 +0300

    Typings: Swiper

--- src/core/components/swiper/swiper.d.ts
+++ src/core/components/swiper/swiper.d.ts
@@ -1,8 +1,17 @@
 import Framework7, { CSSSelector, Framework7EventsClass, Framework7Plugin } from '../app/app-class';
 
 export namespace Swiper {
+  interface Swiper {}
+  interface Parameters {}
   interface AppMethods {
-
+    swiper: {
+      /** initialize slider with options */
+      create(swiperEl: HTMLElement | CSSSelector, params: Parameters): Swiper
+      /** get Swiper instance by HTML element */
+      get(swiperEl: HTMLElement | CSSSelector): Swiper
+      /** destroy Swiper instance */
+      get(swiperEl: HTMLElement | CSSSelector): void
+    }
   }
   interface AppParams {
 
