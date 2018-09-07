1ed9e7d04a49736dc76f081621b3873e36d49b6b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-09-07 15:53:11 +0300

    Typings: Router intro

---
+++ src/core/modules/router/router.d.ts
@@ -0,0 +1,46 @@
+import Framework7, { Framework7EventsClass, Framework7Plugin } from '../../components/app/app-class';
+
+export namespace Router {
+  interface RouteParameters {
+
+  }
+  interface RouteOptions {
+
+  }
+  interface Route {
+
+  }
+  interface Page {
+
+  }
+  interface Router extends Framework7EventsClass<Events> {
+
+  }
+  interface Parameters {
+
+  }
+  interface Events {
+
+  }
+  interface DomEvents {
+
+  }
+
+  interface AppMethods {
+    cache: {
+      xhr: object[],
+      templates: object[],
+      components: object[],
+    }
+  }
+  interface AppParams {
+
+  }
+  interface AppEvents {
+
+  }
+}
+
+declare const RouterModule: Framework7Plugin;
+
+export default RouterModule;
