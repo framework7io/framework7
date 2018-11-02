3d2769f243fbe74675d1bce68066288360e2deb2
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-11-02 19:53:45 +0300

    v4: Phenome - BlockTitle - "large" and "medium" props

--- src/phenome/components/block-title.jsx
+++ src/phenome/components/block-title.jsx
@@ -7,6 +7,8 @@ export default {
     id: [String, Number],
     className: String, // phenome-react-line
     style: Object, // phenome-react-line
+    large: Boolean,
+    medium: Boolean,
     ...Mixins.colorProps,
   },
   render() {
@@ -15,11 +17,17 @@ export default {
       className,
       id,
       style,
+      large,
+      medium,
     } = props;
 
     const classes = Utils.classNames(
       className,
       'block-title',
+      {
+        'block-title-large': large,
+        'block-title-medium': medium,
+      },
       Mixins.colorClasses(props),
     );
     return (<div id={id} style={style} className={classes}><slot /></div>);
