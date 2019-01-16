6d18f848050d7cda0a552114d5dd5fdb8118e1c7
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-01-16 16:21:12 +0300

    v4: Tweak cleanCss

--- scripts/utils/clean-css.js
+++ scripts/utils/clean-css.js
@@ -1,18 +1,17 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 const CleanCSS = require('clean-css');
 
-module.exports = (content, options) => {
-  if (!options) {
-    // eslint-disable-next-line
-    options = {
-      compatibility: '*,-properties.zeroUnits',
-    };
-  }
+module.exports = (content, options = {}) => {
+  // eslint-disable-next-line
+  options = Object.assign({
+    compatibility: '*,-properties.zeroUnits',
+  }, options);
+
   return new Promise((resolve, reject) => {
     if (content instanceof Promise) {
       content.then((c) => {
         const minified = new CleanCSS(options).minify(c);
-        resolve(minified);
+        resolve(minified.styles);
       }).catch((err) => {
         reject(err);
         throw err;
