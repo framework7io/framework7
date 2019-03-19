ef01474f182e068a230758bcd75ade8ed0971648
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-03-19 13:16:31 +0300

    Core: Menu - increase zIndex on open
    Fixes #3080

--- src/core/components/menu/menu.js
+++ src/core/components/menu/menu.js
@@ -6,6 +6,13 @@ const Menu = {
     if (!el) return;
     const $el = $(el).closest('.menu-item-dropdown');
     if (!$el.length) return;
+    const $menuEl = $el.closest('.menu').eq(0);
+    if ($menuEl.length) {
+      const zIndex = $menuEl.css('z-index');
+      const originalZIndex = $menuEl[0].style.zIndex;
+      $menuEl.css('z-index', parseInt(zIndex || 0, 0) + 1);
+      $menuEl[0].f7MenuZIndex = originalZIndex;
+    }
     $el.eq(0).addClass('menu-item-dropdown-opened').trigger('menu:opened');
     app.emit('menuOpened', $el.eq(0)[0]);
   },
@@ -14,6 +21,12 @@ const Menu = {
     if (!el) return;
     const $el = $(el).closest('.menu-item-dropdown-opened');
     if (!$el.length) return;
+    const $menuEl = $el.closest('.menu').eq(0);
+    if ($menuEl.length) {
+      const zIndex = $menuEl[0].f7MenuZIndex;
+      $menuEl.css('z-index', zIndex);
+      delete $menuEl[0].f7MenuZIndex;
+    }
     $el.eq(0).removeClass('menu-item-dropdown-opened').trigger('menu:closed');
     app.emit('menuClosed', $el.eq(0)[0]);
   },
