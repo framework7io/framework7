ce62be80f7758e8bc41c34288502a318825027c6
DAnn2012 <dannunziata@hotmail.it>
2019-03-20 23:18:02 +0100

    Update home.vue
    The list of components in Kitchen Sink is not in alphabetical order

--- kitchen-sink/vue/src/pages/home.vue
+++ kitchen-sink/vue/src/pages/home.vue
@@ -101,10 +101,10 @@
       <f7-list-item link="/lazy-load/" title="Lazy Load">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
-      <f7-list-item link="/list/" title="List View">
+      <f7-list-item link="/list-index/" title="List Index">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
-      <f7-list-item link="/list-index/" title="List Index">
+      <f7-list-item link="/list/" title="List View">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
       <f7-list-item link="/login-screen/" title="Login Screen">
@@ -125,16 +125,16 @@
       <f7-list-item link="/panel/" title="Panel / Side Panels">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
-      <f7-list-item link="/picker/" title="Picker">
-        <f7-icon slot="media" icon="icon-f7"></f7-icon>
-      </f7-list-item>
       <f7-list-item link="/photo-browser/" title="Photo Browser">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
-      <f7-list-item link="/popup/" title="Popup">
+      <f7-list-item link="/picker/" title="Picker">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
       <f7-list-item link="/popover/" title="Popover">
+        <f7-icon slot="media" icon="icon-f7"></f7-icon>
+      </f7-list-item>
+      <f7-list-item link="/popup/" title="Popup">
         <f7-icon slot="media" icon="icon-f7"></f7-icon>
       </f7-list-item>
       <f7-list-item link="/preloader/" title="Preloader">
