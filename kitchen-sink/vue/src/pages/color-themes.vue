e08c8337e4bba36c30b6130611de30b7b5f19ff5
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-07-22 16:15:41 +0300

    v5: Tweak custom vars for fill navbar large transparent

--- kitchen-sink/vue/src/pages/color-themes.vue
+++ kitchen-sink/vue/src/pages/color-themes.vue
@@ -141,6 +141,7 @@
           }
         }
         if (self.barsStyle === 'fill') {
+          const colorThemeRgb = self.$$('html').css('--f7-theme-color-rgb').trim().split(', ');
           styles += `
 /* Invert navigation bars to fill style */
 :root,
@@ -154,9 +155,6 @@
   --f7-bars-border-color: transparent;
   --f7-tabbar-link-active-color: #fff;
   --f7-tabbar-link-inactive-color: rgba(255,255,255,0.54);
-  --f7-searchbar-bg-color: var(--f7-bars-bg-color);
-  --f7-searchbar-input-bg-color: #fff;
-  --f7-searchbar-input-text-color: #000;
   --f7-sheet-border-color: transparent;
   --f7-tabbar-link-active-border-color: #fff;
 }
@@ -170,6 +168,22 @@
   --f7-link-highlight-color: var(--f7-link-highlight-white);
   --f7-button-text-color: #fff;
   --f7-button-pressed-bg-color: rgba(255,255,255,0.1);
+}
+.navbar-inner-large-transparent {
+  --f7-navbar-large-title-text-color: #000;
+
+  --r: ${colorThemeRgb[0]};
+  --g: ${colorThemeRgb[1]};
+  --b: ${colorThemeRgb[2]};
+  --progress: var(--f7-navbar-large-collapse-progress);
+  --f7-bars-link-color: rgb(
+    calc(var(--r) + (255 - var(--r)) * var(--progress)),
+    calc(var(--g) + (255 - var(--g)) * var(--progress)),
+    calc(var(--b) + (255 - var(--b)) * var(--progress))
+  );
+}
+.theme-dark .navbar-inner-large-transparent {
+  --f7-navbar-large-title-text-color: #fff;
 }
           `;
         }
