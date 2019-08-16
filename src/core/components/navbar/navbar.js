70200cfba119adf840f78f3f518ae5c249be7079
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-08-16 17:09:35 +0300

    v5: Navbar - collapse large navbar with subnavbar

--- src/core/components/navbar/navbar.js
+++ src/core/components/navbar/navbar.js
@@ -368,7 +368,7 @@ const Navbar = {
         || ((collapseProgress === 1 && navbarCollapsed) || (collapseProgress === 0 && !navbarCollapsed))
       ) {
         $navbarEl.find('.title').css('opacity', '');
-        $navbarEl.find('.title-large-text').css('transform', '');
+        $navbarEl.find('.title-large-text, .subnavbar').css('transform', '');
         if (isLargeTransparent) {
           $navbarEl.find('.navbar-bg').css('opacity', '');
         } else {
@@ -376,7 +376,7 @@ const Navbar = {
         }
       } else {
         $navbarEl.find('.title').css('opacity', collapseProgress);
-        $navbarEl.find('.title-large-text').css('transform', `translate3d(0px, ${-1 * collapseProgress * navbarTitleLargeHeight}px, 0)`);
+        $navbarEl.find('.title-large-text, .subnavbar').css('transform', `translate3d(0px, ${-1 * collapseProgress * navbarTitleLargeHeight}px, 0)`);
         if (isLargeTransparent) {
           $navbarEl.find('.navbar-bg').css('opacity', collapseProgress);
         } else {
