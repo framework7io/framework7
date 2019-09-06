b18b9d3dca02f183310abd2b58e785a12827b2d4
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-09-06 18:14:45 +0300

    v5: Core: Sortable - add el reference to sorta data

--- src/core/components/sortable/sortable.js
+++ src/core/components/sortable/sortable.js
@@ -173,7 +173,7 @@ const Sortable = {
       }
       if (typeof indexTo !== 'undefined' && !Number.isNaN(indexTo) && indexTo !== indexFrom) {
         $sortingEl.trigger('sortable:sort', { from: indexFrom, to: indexTo });
-        app.emit('sortableSort', $sortingEl[0], { from: indexFrom, to: indexTo });
+        app.emit('sortableSort', $sortingEl[0], { from: indexFrom, to: indexTo, el: $sortingEl[0] }, $sortableContainer[0]);
       }
 
       $insertBeforeEl = undefined;
