3534b78b8d384c5dc9e08721905908e09c9999aa
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-10-21 19:28:24 +0300

    Core: Grid - new resizable grid

--- src/core/components/grid/grid.d.ts
+++ src/core/components/grid/grid.d.ts
@@ -2,13 +2,19 @@ import Framework7, { CSSSelector, Framework7EventsClass, Framework7Plugin } from
 
 export namespace Grid {
   interface AppMethods {
-
+    grid: {
+      /** create/init resizable grid */
+      create(pageEl : HTMLElement | CSSSelector) : void;
+      /** destroy resizable grid */
+      destroy(pageEl : HTMLElement | CSSSelector) : void;
+    }
   }
   interface AppParams {
 
   }
   interface AppEvents {
-
+    /** Event will be triggered when column or row resized */
+    gridResize: (el : HTMLElement) => void
   }
 }
 
