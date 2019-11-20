01dac7fc177f58474ece23db668aba1653eaf8d6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-11-20 16:57:23 +0300

    Phenome: Don't init F7 when it is already initialized

--- src/phenome/components/app.jsx
+++ src/phenome/components/app.jsx
@@ -53,7 +53,7 @@ export default {
     if (parentEl && parentEl !== document.body && parentEl.parentNode === document.body) {
       parentEl.style.height = '100%';
     }
-
+    if (f7.instance) return;
     f7.init(el, params, routes);
   },
 };
