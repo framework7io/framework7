dee6d1daa7d5adf923c97bf4eb2ae2993c44b6cc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-12-21 18:01:47 +0300

    Svelte: Actions components

---
+++ src/svelte/components/actions-label.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  import Mixins from '../utils/mixins';
+  import Utils from '../utils/utils';
+
+  const dispatch = createEventDispatcher();
+
+  export let id = undefined;
+  export let style = undefined;
+
+  let className = undefined;
+  export { className as class };
+
+  export let bold = false;
+
+  $: classes = Utils.classNames(
+    className,
+    'actions-label',
+    {
+      'actions-button-bold': bold,
+    },
+    Mixins.colorClasses($$props),
+  );
+
+  function onClick() {
+    dispatch('click');
+  }
+</script>
+
+<div
+  id={id}
+  style={style}
+  class={classes}
+  on:click={onClick}
+>
+  <slot />
+</div>
