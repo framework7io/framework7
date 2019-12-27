8437c68b69309ec6d948004673b71153a9f371ab
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-12-27 18:04:26 +0300

    Svelte: More F7 Svelte components

---
+++ src/svelte/components/messagebar-sheet.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Mixins from '../utils/mixins';
+  import Utils from '../utils/utils';
+
+  export let id = undefined;
+  export let style = undefined;
+
+  let className = undefined;
+  export { className as class };
+
+  $: classes = Utils.classNames(
+    className,
+    'messagebar-sheet',
+    Mixins.colorClasses($$props),
+  );
+
+</script>
+
+<div id={id} style={style} class={classes}>
+  <slot />
+</div>
