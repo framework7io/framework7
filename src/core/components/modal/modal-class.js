7aa868d0f2e1ce4264cc89469877fd41c9fd105a
zhennann <zhen.nann@icloud.com>
2020-02-24 17:12:44 +0800

    Update modal-class.js
    In some scenarios, the modal.close() is invoked, but modal.destroy() will be invoked before the animationEnd/transitionEnd, so the modal.onClosed() will not be invoked forever

--- src/core/components/modal/modal-class.js
+++ src/core/components/modal/modal-class.js
@@ -233,6 +233,9 @@ class Modal extends Framework7Class {
   destroy() {
     const modal = this;
     if (modal.destroyed) return;
+    if (modal.$el && modal.$el.hasClass('modal-out')) {
+      modal.onClosed();
+    }
     modal.emit(`local::beforeDestroy modalBeforeDestroy ${modal.type}BeforeDestroy`, modal);
     if (modal.$el) {
       modal.$el.trigger(`modal:beforedestroy ${modal.type.toLowerCase()}:beforedestroy`);
