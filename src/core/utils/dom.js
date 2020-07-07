fafbc550447618f38e9d808087c3dec7b246ef27
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-07-07 18:17:15 +0300

    v6: Update to latest Dom7

---
+++ src/core/utils/dom.js
@@ -0,0 +1,8 @@
+import * as methods from 'dom7';
+
+Object.keys(methods).forEach((methodName) => {
+  if (methodName === '$') return;
+  methods.$.fn[methodName] = methods[methodName];
+});
+
+export default methods.$;
