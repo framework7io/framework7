d01c2e14c1778d8eeea1cecacb7c1fc530b3b762
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-07-07 18:16:47 +0300

    v6: Update deps + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,58 @@
 module.exports = {
-  'extends': [
-    'plugin:react/recommended',
-    'airbnb-base',
-  ],
-  'plugins': [
-    'import',
-    'react',
-    'svelte3',
-  ],
-  'globals': {
-    "window": true,
-    "document": true,
-    "XMLHttpRequest": true,
-    "Blob": true,
-    "Document": true,
-    "FormData": true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  plugins: ['react', 'svelte3'],
+  globals: {
+    XMLHttpRequest: true,
+    Blob: true,
+    Document: true,
+    FormData: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   rules: {
-    'max-len': ['error', 1000, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-    'object-curly-newline': ['error', {
-      ObjectExpression: { minProperties: 9, multiline: true, consistent: true },
-      ObjectPattern: { minProperties: 9, multiline: true, consistent: true }
-    }],
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'ignore',
-    }],
-    'no-param-reassign': ['error', {
-      props: false,
-    }],
-    'no-mixed-operators': ['error', {
-      // the list of arthmetic groups disallows mixing `%` and `**`
-      // with other arithmetic operators.
-      groups: [
-        ['%', '**'],
-        ['%', '+'],
-        ['%', '-'],
-        ['%', '*'],
-        ['%', '/'],
-        ['&', '|', '<<', '>>', '>>>'],
-        ['==', '!=', '===', '!=='],
-        ['&&', '||'],
-      ],
-      allowSamePrecedence: false
-    }],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+    'no-mixed-operators': [
+      'error',
+      {
+        // the list of arthmetic groups disallows mixing `%` and `**`
+        // with other arithmetic operators.
+        groups: [
+          ['%', '**'],
+          ['%', '+'],
+          ['%', '-'],
+          ['%', '*'],
+          ['%', '/'],
+          ['&', '|', '<<', '>>', '>>>'],
+          ['==', '!=', '===', '!=='],
+          ['&&', '||'],
+        ],
+        allowSamePrecedence: false,
+      },
+    ],
     'prefer-destructuring': ['off'],
+    'prefer-object-spread': ['off'],
     'prefer-ob': ['off'],
     'react/react-in-jsx-scope': ['off'],
     'react/no-string-refs': ['off'],
     'react/prop-types': ['off'],
-    'linebreak-style': ['off'],
-    'arrow-parens': ['off'],
-    'arrow-body-style': ['off'],
-    'prefer-object-spread': ['off'],
+    'no-restricted-globals': ['error', 'window', 'document'],
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
   },
   overrides: [
     {
@@ -71,7 +62,7 @@ module.exports = {
         'import/no-extraneous-dependencies': ['off'],
         'import/extensions': ['off'],
         'no-dupe-keys': ['off'],
-      }
+      },
     },
     {
       files: ['**/*.svelte'],
@@ -89,7 +80,7 @@ module.exports = {
         'no-nested-ternary': ['off'],
         'a11y-invalid-attribute': ['off'],
         'a11y-missing-attribute': ['off'],
-      }
-    }
+      },
+    },
   ],
 };
