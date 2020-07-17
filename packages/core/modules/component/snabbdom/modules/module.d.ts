6a4e98166cb4ebe714fd6462a194ea1cc595b939
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-07-17 20:07:13 +0300

    Remove package contents

--- packages/core/modules/component/snabbdom/modules/module.d.ts
+++
@@ -1,9 +0,0 @@
-import { PreHook, CreateHook, UpdateHook, DestroyHook, RemoveHook, PostHook } from '../hooks';
-export interface Module {
-    pre: PreHook;
-    create: CreateHook;
-    update: UpdateHook;
-    destroy: DestroyHook;
-    remove: RemoveHook;
-    post: PostHook;
-}
--- packages/core/modules/component/snabbdom/vnode.js
+++
@@ -1,7 +0,0 @@
-export function vnode(sel, data, children, text, elm) {
-    var key = data === undefined ? undefined : data.key;
-    return { sel: sel, data: data, children: children,
-        text: text, elm: elm, key: key };
-}
-export default vnode;
-//# sourceMappingURL=vnode.js.map
