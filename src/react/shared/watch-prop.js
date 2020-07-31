54886e343cd352da36892cf4b113dfaf3954332d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-07-31 21:26:32 +0300

    v6: React - useLayoutEffect for watchProp

--- src/react/shared/watch-prop.js
+++ src/react/shared/watch-prop.js
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
 export const watchProp = (value, callback) => {
   const valueRef = useRef(value);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (value !== valueRef.current && callback) {
       callback(value, valueRef.current);
     }
