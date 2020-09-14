eab86962b1f2213d0e5edd4705c0a67fb17031ee
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-09-14 16:05:03 +0300

    Update modal-state-classes.js

--- src/vue/shared/modal-state-classes.js
+++ src/vue/shared/modal-state-classes.js
@@ -1,6 +1,6 @@
 export const modalStateClasses = ({ isOpened, isClosing } = {}) => {
   return {
-    'modal-in': isOpened.current && !isClosing.current,
-    'modal-out': isClosing.current,
+    'modal-in': isOpened.value && !isClosing.value,
+    'modal-out': isClosing.value,
   };
 };
