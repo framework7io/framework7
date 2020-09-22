5cadf2d209aea6f00c1f6220d19352b246bfdc4a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-09-22 18:29:32 +0300

    Update messages.jsx

--- src/react/components/messages.jsx
+++ src/react/components/messages.jsx
@@ -14,6 +14,7 @@ import { watchProp } from '../shared/watch-prop';
   newMessagesFirst? : boolean
   scrollMessages? : boolean
   scrollMessagesOnEdge? : boolean
+  typing?: boolean
   firstMessageRule? : Function
   lastMessageRule? : Function
   tailMessageRule? : Function
