9d3f18a695289a9674260a86baa0733b50cd5263
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-09-29 18:53:23 +0300

    v6: Svelte new app init

--- src/svelte/components/app.svelte
+++ src/svelte/components/app.svelte
@@ -1,14 +1,11 @@
 <script>
   import { onMount } from 'svelte';
   import { f7, f7init } from '../shared/f7';
-  import RoutableModals from './routable-modals';
   import { colorClasses } from '../shared/mixins';
-  import { classNames } from '../shared/utils';
+  import { classNames, noUndefinedProps } from '../shared/utils';
   import { restProps } from '../shared/rest-props';
 
-  export let id = 'framework7-root';
-  export let params = {};
-  export let routes = [];
+  import RoutableModals from './routable-modals';
 
   let className = undefined;
   export { className as class };
@@ -17,17 +14,24 @@
 
   $: classes = classNames(className, 'framework7-root', colorClasses($$props));
 
+  if (!f7 || typeof window === 'undefined') {
+    f7init(el, noUndefinedProps($$props), false);
+  }
+
   onMount(() => {
     const parentEl = el.parentNode;
     if (parentEl && parentEl !== document.body && parentEl.parentNode === document.body) {
       parentEl.style.height = '100%';
     }
-    if (f7) return;
-    f7init(el, params, routes);
+    if (f7) {
+      f7.init(el);
+      return;
+    }
+    f7init(el, noUndefinedProps($$props), true);
   });
 </script>
 
-<div {id} bind:this={el} class={classes} {...restProps($$restProps)}>
+<div bind:this={el} class={classes} {...restProps($$restProps)}>
   <slot />
   <RoutableModals />
 </div>
