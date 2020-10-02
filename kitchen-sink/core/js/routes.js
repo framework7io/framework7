2d3ad4419d4618d288379dc4fc36dc6eb7be2d92
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-10-02 14:47:53 +0300

    v6: Update KS Core with new components

--- kitchen-sink/core/js/routes.js
+++ kitchen-sink/core/js/routes.js
@@ -14,53 +14,53 @@ var routes = [
   // Right Panel pages
   {
     path: '/panel-right-1/',
-    content: '\
-      <div class="page">\
-        <div class="navbar">\
-          <div class="navbar-bg"></div>\
-          <div class="navbar-inner sliding">\
-            <div class="left">\
-              <a href="#" class="link back">\
-                <i class="icon icon-back"></i>\
-                <span class="if-not-md">Back</span>\
-              </a>\
-            </div>\
-            <div class="title">Panel Page 1</div>\
-          </div>\
-        </div>\
-        <div class="page-content">\
-          <div class="block">\
-            <p>This is a right panel page 1</p>\
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
-          </div>\
-        </div>\
-      </div>\
-    ',
+    content: `
+      <div class="page">
+        <div class="navbar">
+          <div class="navbar-bg"></div>
+          <div class="navbar-inner sliding">
+            <div class="left">
+              <a href="#" class="link back">
+                <i class="icon icon-back"></i>
+                <span class="if-not-md">Back</span>
+              </a>
+            </div>
+            <div class="title">Panel Page 1</div>
+          </div>
+        </div>
+        <div class="page-content">
+          <div class="block">
+            <p>This is a right panel page 1</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>
+          </div>
+        </div>
+      </div>
+    `,
   },
   {
     path: '/panel-right-2/',
-    content: '\
-      <div class="page">\
-        <div class="navbar">\
-          <div class="navbar-bg"></div>\
-          <div class="navbar-inner sliding">\
-            <div class="left">\
-              <a href="#" class="link back">\
-                <i class="icon icon-back"></i>\
-                <span class="if-not-md">Back</span>\
-              </a>\
-            </div>\
-            <div class="title">Panel Page 2</div>\
-          </div>\
-        </div>\
-        <div class="page-content">\
-          <div class="block">\
-            <p>This is a right panel page 2</p>\
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
-          </div>\
-        </div>\
-      </div>\
-    ',
+    content: `
+      <div class="page">
+        <div class="navbar">
+          <div class="navbar-bg"></div>
+          <div class="navbar-inner sliding">
+            <div class="left">
+              <a href="#" class="link back">
+                <i class="icon icon-back"></i>
+                <span class="if-not-md">Back</span>
+              </a>
+            </div>
+            <div class="title">Panel Page 2</div>
+          </div>
+        </div>
+        <div class="page-content">
+          <div class="block">
+            <p>This is a right panel page 2</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>
+          </div>
+        </div>
+      </div>
+    `,
   },
 
   // Components
@@ -82,7 +82,7 @@ var routes = [
   },
   {
     path: '/badge/',
-    componentUrl: './pages/badge.html',
+    url: './pages/badge.html',
   },
   {
     path: '/buttons/',
@@ -389,44 +389,44 @@ var routes = [
       {
         path: '/',
         id: 'tab1',
-        content: ' \
-        <div class="block"> \
-          <p>Tab 1 content</p> \
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p> \
-          <p>Saepe explicabo voluptas ducimus provident, doloremque quo totam molestias! Suscipit blanditiis eaque exercitationem praesentium reprehenderit, fuga accusamus possimus sed, sint facilis ratione quod, qui dignissimos voluptas! Aliquam rerum consequuntur deleniti.</p> \
-          <p>Totam reprehenderit amet commodi ipsum nam provident doloremque possimus odio itaque, est animi culpa modi consequatur reiciendis corporis libero laudantium sed eveniet unde delectus a maiores nihil dolores? Natus, perferendis.</p> \
-        </div> \
-        ',
+        content: `
+        <div class="block">
+          <p>Tab 1 content</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p>
+          <p>Saepe explicabo voluptas ducimus provident, doloremque quo totam molestias! Suscipit blanditiis eaque exercitationem praesentium reprehenderit, fuga accusamus possimus sed, sint facilis ratione quod, qui dignissimos voluptas! Aliquam rerum consequuntur deleniti.</p>
+          <p>Totam reprehenderit amet commodi ipsum nam provident doloremque possimus odio itaque, est animi culpa modi consequatur reiciendis corporis libero laudantium sed eveniet unde delectus a maiores nihil dolores? Natus, perferendis.</p>
+        </div>
+        `,
       },
       {
         path: '/tab2/',
         id: 'tab2',
-        content: '\
-        <div class="block"> \
-          <p>Tab 2 content</p> \
-          <p>Suscipit, facere quasi atque totam. Repudiandae facilis at optio atque, rem nam, natus ratione cum enim voluptatem suscipit veniam! Repellat, est debitis. Modi nam mollitia explicabo, unde aliquid impedit! Adipisci!</p> \
-          <p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p> \
-          <p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p> \
-        </div> \
-        ',
+        content: `
+        <div class="block">
+          <p>Tab 2 content</p>
+          <p>Suscipit, facere quasi atque totam. Repudiandae facilis at optio atque, rem nam, natus ratione cum enim voluptatem suscipit veniam! Repellat, est debitis. Modi nam mollitia explicabo, unde aliquid impedit! Adipisci!</p>
+          <p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p>
+          <p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p>
+        </div>
+        `,
       },
       {
         path: '/tab3/',
         id: 'tab3',
-        content: '\
-        <div class="block"> \
-          <p>Tab 3 content</p> \
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p> \
-          <p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p> \
-          <p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p> \
-        </div> \
-        ',
+        content: `
+        <div class="block">
+          <p>Tab 3 content</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p>
+          <p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p>
+          <p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p>
+        </div>
+        `,
       },
     ],
   },
   {
     path: '/text-editor/',
-    componentUrl: './pages/text-editor.html'
+    componentUrl: './pages/text-editor.html',
   },
   {
     path: '/toast/',
@@ -541,7 +541,7 @@ var routes = [
         path: '/master-detail/:id/',
         templateUrl: './pages/master-detail-detail.html',
       },
-    ]
+    ],
   },
 
   // Default route (404 page). MUST BE THE LAST
