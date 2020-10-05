7391cadf0f66b4977b3a1aa0a971498cbf445557
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-10-05 15:39:48 +0300

    v6: Svelte - AreaChart & PieChart components

---
+++ src/svelte/components/area-chart.svelte
@@ -0,0 +1,354 @@
+<script>
+  import { createEventDispatcher, onDestroy, onMount } from 'svelte';
+  import { classNames, createEmitter } from '../shared/utils';
+  import { restProps } from '../shared/rest-props';
+  import { f7 } from '../shared/f7';
+
+  const emit = createEmitter(createEventDispatcher, $$props);
+
+  let className = undefined;
+  export { className as class };
+
+  export let lineChart = false;
+  export let datasets = [];
+  export let axis = false;
+  export let axisLabels = [];
+  export let tooltip = false;
+  export let legend = false;
+  export let toggleDatasets = false;
+  export let width = 640;
+  export let height = 320;
+  export let maxAxisLabels = 8;
+
+  let formatAxisLabelProp = undefined;
+  export { formatAxisLabelProp as formatAxisLabel };
+  let formatLegendLabelProp = undefined;
+  export { formatLegendLabelProp as formatLegendLabel };
+  let formatTooltipProp = undefined;
+  export { formatTooltipProp as formatTooltip };
+  export let formatTooltipAxisLabel = undefined;
+  export let formatTooltipTotal = undefined;
+  export let formatTooltipDataset = undefined;
+
+  let el;
+  let svgEl = null;
+  let currentIndex = null;
+  let previousIndex = null;
+  let hiddenDatasets = [];
+  let f7Tooltip = null;
+  let linesOffsets = null;
+
+  const setCurrentIndex = (value) => {
+    previousIndex = currentIndex;
+    currentIndex = value;
+  };
+  const setHiddenDatasets = (value) => {
+    hiddenDatasets = value;
+  };
+
+  const getVisibleLegends = () => {
+    if (!maxAxisLabels || axisLabels.length <= maxAxisLabels) return axisLabels;
+    const skipStep = Math.ceil(axisLabels.length / maxAxisLabels);
+    const filtered = axisLabels.filter((label, index) => index % skipStep === 0);
+    return filtered;
+  };
+
+  const getSummValues = () => {
+    const summValues = [];
+    datasets
+      .filter((dataset, index) => !hiddenDatasets.includes(index))
+      .forEach(({ values }) => {
+        values.forEach((value, valueIndex) => {
+          if (!summValues[valueIndex]) summValues[valueIndex] = 0;
+          summValues[valueIndex] += value;
+        });
+      });
+    return summValues;
+  };
+
+  const getChartData = () => {
+    const data = [];
+    if (!datasets.length) {
+      return data;
+    }
+    const lastValues = datasets[0].values.map(() => 0);
+    let maxValue = 0;
+    if (lineChart) {
+      datasets.forEach(({ values }) => {
+        const datasetMaxValue = Math.max(...values);
+        if (datasetMaxValue > maxValue) maxValue = datasetMaxValue;
+      });
+    } else {
+      maxValue = Math.max(...getSummValues());
+    }
+
+    datasets
+      .filter((dataset, index) => !hiddenDatasets.includes(index))
+      .forEach(({ label, values, color }) => {
+        const points = values.map((originalValue, valueIndex) => {
+          lastValues[valueIndex] += originalValue;
+          const value = lineChart ? originalValue : lastValues[valueIndex];
+          const x = (valueIndex / (values.length - 1)) * width;
+          const y = height - (value / maxValue) * height;
+          if (lineChart) {
+            return `${valueIndex === 0 ? 'M' : 'L'}${x},${y}`;
+          }
+          return `${x} ${y}`;
+        });
+        if (!lineChart) {
+          points.push(`${width} ${height} 0 ${height}`);
+        }
+
+        data.push({
+          label,
+          points: points.join(' '),
+          color,
+        });
+      });
+    return data.reverse();
+  };
+
+  const getVerticalLines = () => {
+    const lines = [];
+    if (!datasets.length) {
+      return lines;
+    }
+    const values = datasets[0].values;
+    values.forEach((value, valueIndex) => {
+      const x = (valueIndex / (values.length - 1)) * width;
+      lines.push(x);
+    });
+    return lines;
+  };
+
+  const toggleDataset = (index) => {
+    if (!toggleDatasets) return;
+    if (hiddenDatasets.includes(index)) {
+      hiddenDatasets.splice(hiddenDatasets.indexOf(index), 1);
+    } else {
+      hiddenDatasets.push(index);
+    }
+    setHiddenDatasets([...hiddenDatasets]);
+  };
+
+  const formatAxisLabel = (label) => {
+    if (formatAxisLabelProp) return formatAxisLabelProp(label);
+    return label;
+  };
+
+  const formatLegendLabel = (label) => {
+    if (formatLegendLabelProp) return formatLegendLabelProp(label);
+    return label;
+  };
+
+  const calcLinesOffsets = () => {
+    const lines = svgEl.querySelectorAll('line');
+    linesOffsets = [];
+    for (let i = 0; i < lines.length; i += 1) {
+      linesOffsets.push(lines[i].getBoundingClientRect().left);
+    }
+  };
+
+  const formatTooltip = () => {
+    if (currentIndex === null) return '';
+    let total = 0;
+    const currentValues = datasets
+      .filter((dataset, index) => !hiddenDatasets.includes(index))
+      .map((dataset) => ({
+        color: dataset.color,
+        label: dataset.label,
+        value: dataset.values[currentIndex],
+      }));
+    currentValues.forEach((dataset) => {
+      total += dataset.value;
+    });
+    if (formatTooltipProp) {
+      return formatTooltipProp({
+        index: currentIndex,
+        total,
+        datasets: currentValues,
+      });
+    }
+    let labelText = formatTooltipAxisLabel
+      ? formatTooltipAxisLabel(axisLabels[currentIndex])
+      : formatAxisLabel(axisLabels[currentIndex]);
+    if (!labelText) labelText = '';
+    const totalText = formatTooltipTotal ? formatTooltipTotal(total) : total;
+    // prettier-ignore
+    const datasetsText = currentValues.length > 0 ? `
+      <ul class="area-chart-tooltip-list">
+        ${currentValues
+          .map(({ label, color, value }) => {
+            const valueText = formatTooltipDataset
+              ? formatTooltipDataset(label, value, color)
+              : `${label}: ${value}`;
+            return `
+              <li><span style="background-color: ${color};"></span>${valueText}</li>
+            `;
+          }).join('')}
+      </ul>` : '';
+    // prettier-ignore
+    return `
+      <div class="area-chart-tooltip-label">${labelText}</div>
+      <div class="area-chart-tooltip-total">${totalText}</div>
+      ${datasetsText}
+    `;
+  };
+
+  const setTooltip = () => {
+    if (!tooltip) return;
+    if (currentIndex === null && !f7Tooltip) return;
+    const hasVisibleDataSets =
+      datasets.filter((dataset, index) => !hiddenDatasets.includes(index)).length > 0;
+    if (!hasVisibleDataSets) {
+      if (f7Tooltip && f7Tooltip.hide) f7Tooltip.hide();
+      return;
+    }
+
+    if (currentIndex !== null && !f7Tooltip) {
+      f7Tooltip = f7.tooltip.create({
+        trigger: 'manual',
+        containerEl: el,
+        targetEl: svgEl.querySelector(`line[data-index="${currentIndex}"]`),
+        text: formatTooltip(),
+        cssClass: 'area-chart-tooltip',
+      });
+      if (f7Tooltip && f7Tooltip.show) {
+        f7Tooltip.show();
+      }
+      return;
+    }
+    if (!f7Tooltip || !f7Tooltip.hide || !f7Tooltip.show) {
+      return;
+    }
+    if (currentIndex !== null) {
+      f7Tooltip.setText(formatTooltip());
+      f7Tooltip.targetEl = svgEl.querySelector(`line[data-index="${currentIndex}"]`);
+      f7Tooltip.$targetEl = f7.$(f7Tooltip.targetEl);
+      f7Tooltip.show();
+    } else {
+      f7Tooltip.hide();
+    }
+  };
+
+  const onMouseEnter = () => {
+    calcLinesOffsets();
+  };
+
+  const onMouseMove = (e) => {
+    if (!linesOffsets) {
+      calcLinesOffsets();
+    }
+    let currentLeft = e.pageX;
+    if (typeof currentLeft === 'undefined') currentLeft = 0;
+    const distances = linesOffsets.map((left) => Math.abs(currentLeft - left));
+    const minDistance = Math.min(...distances);
+    const closestIndex = distances.indexOf(minDistance);
+    setCurrentIndex(closestIndex);
+  };
+
+  const onMouseLeave = () => {
+    setCurrentIndex(null);
+  };
+
+  const attachEvents = () => {
+    if (!svgEl) return;
+    svgEl.addEventListener('mouseenter', onMouseEnter);
+    svgEl.addEventListener('mousemove', onMouseMove);
+    svgEl.addEventListener('mouseleave', onMouseLeave);
+  };
+
+  const detachEvents = () => {
+    if (!svgEl) return;
+    svgEl.removeEventListener('mouseenter', onMouseEnter);
+    svgEl.removeEventListener('mousemove', onMouseMove);
+    svgEl.removeEventListener('mouseleave', onMouseLeave);
+  };
+
+  onMount(() => {
+    attachEvents();
+  });
+
+  onDestroy(() => {
+    detachEvents();
+    if (f7Tooltip && f7Tooltip.destroy) {
+      f7Tooltip.destroy();
+    }
+    f7Tooltip = null;
+  });
+
+  const watchCurrentIndex = () => {
+    if (currentIndex === previousIndex) return;
+    emit('select', [currentIndex]);
+    setTooltip();
+  };
+
+  $: watchCurrentIndex(currentIndex);
+  $: classes = classNames('area-chart', className);
+
+  $: chartData = getChartData(datasets, hiddenDatasets);
+  $: verticalLines = getVerticalLines(datasets);
+  $: visibleLegends = getVisibleLegends(maxAxisLabels, axisLabels);
+</script>
+
+<div bind:this={el} class={classes} {...restProps($$restProps)}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    {width}
+    {height}
+    viewBox={`0 0 ${width} ${height}`}
+    preserveAspectRatio="none"
+    bind:this={svgEl}>
+    {#each chartData as data (data.label)}
+      {#if lineChart}
+        <path stroke={data.color} fillRule="evenodd" d={data.points} />
+      {:else}
+        <polygon fill={data.color} fillRule="evenodd" points={data.points} />
+      {/if}
+    {/each}
+
+    {#each verticalLines as line, index (index)}
+      <line
+        data-index={index}
+        fill="#000"
+        x1={line}
+        y1={0}
+        x2={line}
+        y2={height}
+        class={classNames({ 'area-chart-current-line': currentIndex === index })} />
+    {/each}
+  </svg>
+  {#if axis}
+    <div class="area-chart-axis">
+      {#each axisLabels as label, index (index)}
+        <span>
+          {#if visibleLegends.includes(label)}<span>{formatAxisLabel(label)}</span>{/if}
+        </span>
+      {/each}
+    </div>
+  {/if}
+  {#if legend}
+    <div class="area-chart-legend">
+      {#each datasets as dataset, index}
+        {#if toggleDatasets}
+          <button
+            class={classNames('area-chart-legend-item', {
+              'area-chart-legend-item-hidden': hiddenDatasets.includes(index),
+              'area-chart-legend-button': toggleDatasets,
+            })}
+            type="button"
+            on:click={() => toggleDataset(index)}>
+            <span style={`background-color: ${dataset.color}`} />
+            {formatLegendLabel(dataset.label)}
+          </button>
+        {:else}
+          <span class="area-chart-legend-item">
+            <span style={`background-color: ${dataset.color}`} />
+            {formatLegendLabel(dataset.label)}
+          </span>
+        {/if}
+      {/each}
+    </div>
+  {/if}
+  <slot />
+</div>
---
+++ src/svelte/components/pie-chart.svelte
@@ -0,0 +1,172 @@
+<script>
+  import { createEventDispatcher, onDestroy } from 'svelte';
+  import { classNames, createEmitter } from '../shared/utils';
+  import { restProps } from '../shared/rest-props';
+  import { f7 } from '../shared/f7';
+
+  const emit = createEmitter(createEventDispatcher, $$props);
+
+  let className = undefined;
+  export { className as class };
+
+  export let size = 320;
+  export let tooltip = false;
+  export let datasets = [];
+  export let formatTooltip = undefined;
+
+  let el;
+  let currentIndex = null;
+  let previousIndex = null;
+  let f7Tooltip = null;
+  let timeout = null;
+
+  const setCurrentIndex = (index) => {
+    if (index === null) {
+      timeout = setTimeout(() => {
+        previousIndex = currentIndex;
+        currentIndex = index;
+      });
+    } else {
+      clearTimeout(timeout);
+      previousIndex = currentIndex;
+      currentIndex = index;
+    }
+  };
+
+  const getSummValue = () => {
+    let summ = 0;
+    datasets
+      .map((d) => d.value || 0)
+      .forEach((value) => {
+        summ += value;
+      });
+    return summ;
+  };
+
+  const getPaths = () => {
+    const paths = [];
+
+    let cumulativePercentage = 0;
+
+    function getCoordinatesForPercentage(percentage) {
+      const x = Math.cos(2 * Math.PI * percentage) * (size / 3);
+      const y = Math.sin(2 * Math.PI * percentage) * (size / 3);
+      return [x, y];
+    }
+
+    datasets.forEach(({ value, label, color }) => {
+      const percentage = value / getSummValue();
+
+      const [startX, startY] = getCoordinatesForPercentage(cumulativePercentage);
+      cumulativePercentage += percentage;
+      const [endX, endY] = getCoordinatesForPercentage(cumulativePercentage);
+      const largeArcFlag = percentage > 0.5 ? 1 : 0;
+      const points = [
+        `M ${startX} ${startY}`, // Move
+        `A ${size / 3} ${size / 3} 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
+        'L 0 0', // Line
+      ].join(' ');
+
+      paths.push({
+        points,
+        label,
+        color,
+      });
+    });
+    return paths;
+  };
+
+  const formatTooltipText = () => {
+    if (currentIndex === null) return '';
+    const { value, label, color } = datasets[currentIndex];
+    const percentage = (value / getSummValue()) * 100;
+
+    const round = (v) => {
+      if (parseInt(v, 10) === v) return v;
+      return Math.round(v * 100) / 100;
+    };
+
+    if (formatTooltip) {
+      return formatTooltip({
+        index: currentIndex,
+        value,
+        label,
+        color,
+        percentage,
+      });
+    }
+
+    const tooltipText = `${label ? `${label}: ` : ''}${round(value)} (${round(percentage)}%)`;
+
+    return `
+      <div class="pie-chart-tooltip-label">
+        <span class="pie-chart-tooltip-color" style="background-color: ${color};"></span> ${tooltipText}
+      </div>
+    `;
+  };
+
+  const setTooltip = () => {
+    if (currentIndex === null && !f7Tooltip) return;
+    if (!tooltip || !el || !f7) return;
+    if (currentIndex !== null && !f7Tooltip) {
+      f7Tooltip = f7.tooltip.create({
+        trigger: 'manual',
+        containerEl: el,
+        targetEl: el.querySelector(`path[data-index="${currentIndex}"]`),
+        text: formatTooltipText(),
+        cssClass: 'pie-chart-tooltip',
+      });
+      f7Tooltip.show();
+      return;
+    }
+    if (!f7Tooltip) return;
+    if (currentIndex !== null) {
+      f7Tooltip.setText(formatTooltipText());
+      f7Tooltip.targetEl = el.querySelector(`path[data-index="${currentIndex}"]`);
+      f7Tooltip.$targetEl = f7.$(el.querySelector(`path[data-index="${currentIndex}"]`));
+
+      f7Tooltip.show();
+    } else {
+      f7Tooltip.hide();
+    }
+  };
+
+  const watchCurrentIndex = () => {
+    if (currentIndex === previousIndex) return;
+    emit('select', [currentIndex, datasets[currentIndex]]);
+    setTooltip();
+  };
+
+  $: classes = classNames('pie-chart', className);
+  $: paths = getPaths(datasets);
+
+  $: watchCurrentIndex(currentIndex);
+
+  onDestroy(() => {
+    if (f7Tooltip && f7Tooltip.destroy) {
+      f7Tooltip.destroy();
+    }
+    f7Tooltip = null;
+  });
+</script>
+
+<div bind:this={el} class={classes} {...restProps($$restProps)}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox={`-${size / 3} -${size / 3} ${(size * 2) / 3} ${(size * 2) / 3}`}
+    style="transform: rotate(-90deg)">
+    {#each paths as path, index (index)}
+      <path
+        d={path.points}
+        fill={path.color}
+        data-index={index}
+        class={classNames({ 'pie-chart-hidden': currentIndex !== null && currentIndex !== index })}
+        on:click={() => setCurrentIndex(index)}
+        on:mouseenter={() => setCurrentIndex(index)}
+        on:mouseleave={() => setCurrentIndex(null)} />
+    {/each}
+  </svg>
+  <slot />
+</div>
