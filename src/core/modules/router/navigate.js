92232160da9cfaa11afb2513394a3da2f52ec3f9
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-11-20 15:22:26 +0300

    Core: Router - allow to have `openIn` in route options

--- src/core/modules/router/navigate.js
+++ src/core/modules/router/navigate.js
@@ -819,20 +819,20 @@ function openIn(router, url, options) {
     ...options,
   };
   if (options.openIn === 'popup') {
-    params.content = `<div class="popup popup-router-open-in" data-url="${url}"><div class="view view-init" data-url="${url}"></div></div>`;
+    params.content = `<div class="popup popup-router-open-in" data-url="${url}"><div class="view view-init" data-links-view="${router.view.selector}" data-url="${url}" data-ignore-open-in="true"></div></div>`;
     navigateOptions.route.popup = params;
   }
   if (options.openIn === 'loginScreen') {
-    params.content = `<div class="login-screen login-screen-router-open-in" data-url="${url}"><div class="view view-init" data-url="${url}"></div></div>`;
+    params.content = `<div class="login-screen login-screen-router-open-in" data-url="${url}"><div class="view view-init" data-links-view="${router.view.selector}" data-url="${url}" data-ignore-open-in="true"></div></div>`;
     navigateOptions.route.loginScreen = params;
   }
   if (options.openIn === 'sheet') {
-    params.content = `<div class="sheet-modal sheet-modal-router-open-in" data-url="${url}"><div class="sheet-modal-inner"><div class="view view-init" data-url="${url}"></div></div></div>`;
+    params.content = `<div class="sheet-modal sheet-modal-router-open-in" data-url="${url}"><div class="sheet-modal-inner"><div class="view view-init" data-links-view="${router.view.selector}" data-url="${url}" data-ignore-open-in="true"></div></div></div>`;
     navigateOptions.route.sheet = params;
   }
   if (options.openIn === 'popover') {
     params.targetEl = options.clickedEl || options.targetEl;
-    params.content = `<div class="popover popover-router-open-in" data-url="${url}"><div class="popover-inner"><div class="view view-init" data-url="${url}"></div></div></div>`;
+    params.content = `<div class="popover popover-router-open-in" data-url="${url}"><div class="popover-inner"><div class="view view-init" data-links-view="${router.view.selector}" data-url="${url}" data-ignore-open-in="true"></div></div></div>`;
     navigateOptions.route.popover = params;
   }
   if (options.openIn.indexOf('panel') >= 0) {
@@ -840,7 +840,7 @@ function openIn(router, url, options) {
     const side = parts[1] || 'left';
     const effect = parts[2] || 'cover';
     params.targetEl = options.clickedEl || options.targetEl;
-    params.content = `<div class="panel panel-router-open-in panel-${side} panel-${effect}" data-url="${url}"><div class="view view-init" data-url="${url}"></div></div>`;
+    params.content = `<div class="panel panel-router-open-in panel-${side} panel-${effect}" data-url="${url}"><div class="view view-init" data-links-view="${router.view.selector}" data-url="${url}" data-ignore-open-in="true"></div></div>`;
     navigateOptions.route.panel = params;
   }
   return router.navigate(navigateOptions);
@@ -922,7 +922,10 @@ function navigate(navigateParams, navigateOptions = {}) {
     extend(options, navigateOptions);
   }
 
-  if (options.openIn) {
+  if (
+    options.openIn &&
+    (!router.params.ignoreOpenIn || (router.params.ignoreOpenIn && router.history.length > 0))
+  ) {
     return openIn(router, navigateUrl, options);
   }
 
