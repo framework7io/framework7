ae095b06606b389cacfe0f213b2d51bab9440872
DAnn2012 <dannunziata@hotmail.it>
2021-03-01 15:54:20 +0100

    Update gauge.jsx

--- src/react/components/gauge.jsx
+++ src/react/components/gauge.jsx
@@ -9,9 +9,7 @@ import { classNames, getExtraAttrs } from '../shared/utils';
   type? : string
   value? : number | string
   size? : number | string
-  bgColor? : string
   borderBgColor? : string
-  borderColor? : string
   borderWidth? : number | string
   valueText? : number | string
   valueTextColor? : string
