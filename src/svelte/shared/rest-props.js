5c4ea267e42633827b2790b2105e00a7a7b7d574
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-07-26 11:33:51 +0300

    fix(svelte): ignore some predefined rest props

--- src/svelte/shared/rest-props.js
+++ src/svelte/shared/rest-props.js
@@ -1,9 +1,58 @@
+const ignoreProps = [
+  'color',
+  'colorTheme',
+  'textColor',
+  'bgColor',
+  'borderColor',
+  'rippleColor',
+  'themeDark',
+  'force',
+  'reloadCurrent',
+  'reloadPrevious',
+  'reloadAll',
+  'reloadDetail',
+  'animate',
+  'ignoreCache',
+  'routeTabId',
+  'view',
+  'transition',
+  'openIn',
+  'back',
+  'linkBack',
+  'external',
+  'preventRouter',
+  'searchbarEnable',
+  'searchbarDisable',
+  'searchbarClear',
+  'searchbarToggle',
+  'panelOpen',
+  'panelClose',
+  'panelToggle',
+  'popupOpen',
+  'popupClose',
+  'actionsOpen',
+  'actionsClose',
+  'popoverOpen',
+  'popoverClose',
+  'loginScreenOpen',
+  'loginScreenClose',
+  'sheetOpen',
+  'sheetClose',
+  'sortableEnable',
+  'sortableDisable',
+  'sortableToggle',
+  'cardOpen',
+  'cardClose',
+];
+
 export function restProps(rest = {}) {
   const props = {};
-  Object.keys(rest).forEach((key) => {
-    if (key.indexOf('on') !== 0) {
-      props[key] = rest[key];
-    }
-  });
+  Object.keys(rest)
+    .filter((prop) => !ignoreProps.includes(prop))
+    .forEach((key) => {
+      if (key.indexOf('on') !== 0) {
+        props[key] = rest[key];
+      }
+    });
   return props;
 }
