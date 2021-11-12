e9d0db713efee6951f02d90d41a69c065a7df93f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-11-12 14:09:23 +0300

    fix(preloader): add `showIn` and `hideIn` to types
    fixes #3956

--- src/core/components/preloader/preloader.d.ts
+++ src/core/components/preloader/preloader.d.ts
@@ -5,8 +5,12 @@ export namespace Preloader {
     preloader: {
       /** Show Preloader overlay */
       show(color?: string | 'white'): void;
+      /** Show Preloader overlay in specified element */
+      showIn(el: HTMLElement | CSSSelector, color?: string): void;
       /** Hide Preloader overlay */
       hide(): void;
+      /** Hide Preloader overlay in specified element */
+      hideIn(el: HTMLElement | CSSSelector): void;
     };
   }
   interface AppParams {}
