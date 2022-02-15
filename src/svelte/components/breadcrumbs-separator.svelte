aa74c2550772c97bd1c5ed1aa88a28aff50410b3
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-02-15 13:31:29 +0300

    feat(svelte): new breadcrumbs component

---
+++ src/svelte/components/breadcrumbs-separator.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { classNames } from '../shared/utils.js';
+  import { restProps } from '../shared/rest-props.js';
+
+  let className = undefined;
+  export { className as class };
+
+  $: classes = classNames(className, 'breadcrumbs-separator');
+</script>
+
+<div class={classes} {...restProps($$restProps)} />
---
+++ src/svelte/components/breadcrumbs.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { classNames } from '../shared/utils.js';
+  import { restProps } from '../shared/rest-props.js';
+
+  let className = undefined;
+  export { className as class };
+
+  $: classes = classNames(className, 'breadcrumbs');
+</script>
+
+<div class={classes} {...restProps($$restProps)}>
+  <slot />
+</div>
