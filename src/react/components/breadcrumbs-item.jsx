49e8f3530a173a297313c4daca6350bcd0e975a5
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-02-15 13:09:50 +0300

    feat(react): new breadcrumbs component

---
+++ src/react/components/breadcrumbs-item.jsx
@@ -0,0 +1,44 @@
+import React, { forwardRef, useRef, useImperativeHandle } from 'react';
+import { classNames, getExtraAttrs, emit } from '../shared/utils.js';
+
+/* dts-props
+  id?: string | number;
+  className?: string;
+  style?: React.CSSProperties;
+  onClick? : (event?: any) => void;
+  active?: boolean;
+  ref?: React.MutableRefObject<{el: HTMLElement | null}>;
+*/
+
+const BreadcrumbsItem = forwardRef((props, ref) => {
+  const { className, id, style, active, children } = props;
+
+  const extraAttrs = getExtraAttrs(props);
+
+  const elRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    el: elRef.current,
+  }));
+
+  const onClick = (e) => {
+    emit(props, 'click', e);
+  };
+
+  return (
+    <div
+      className={classNames('breadcrumbs-item', className, active && 'breadcrumbs-item-active')}
+      ref={elRef}
+      id={id}
+      style={style}
+      onClick={onClick}
+      {...extraAttrs}
+    >
+      {children}
+    </div>
+  );
+});
+
+BreadcrumbsItem.displayName = 'f7-breadcrumbs-item';
+
+export default BreadcrumbsItem;
---
+++ src/react/components/breadcrumbs.jsx
@@ -0,0 +1,37 @@
+import React, { forwardRef, useRef, useImperativeHandle } from 'react';
+import { classNames, getExtraAttrs } from '../shared/utils.js';
+
+/* dts-props
+  id?: string | number;
+  className?: string;
+  style?: React.CSSProperties;
+  ref?: React.MutableRefObject<{el: HTMLElement | null}>;
+*/
+
+const Breadcrumbs = forwardRef((props, ref) => {
+  const { className, id, style, children } = props;
+
+  const extraAttrs = getExtraAttrs(props);
+
+  const elRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    el: elRef.current,
+  }));
+
+  return (
+    <div
+      className={classNames('breadcrumbs', className)}
+      ref={elRef}
+      id={id}
+      style={style}
+      {...extraAttrs}
+    >
+      {children}
+    </div>
+  );
+});
+
+Breadcrumbs.displayName = 'f7-breadcrumbs';
+
+export default Breadcrumbs;
