e4cb1505573f69b4c1addc5b651853991606c1cd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-10-27 16:27:10 +0300

    feat(list): support `label.item-content` as activable element

--- src/core/modules/touch/touch.js
+++ src/core/modules/touch/touch.js
@@ -517,7 +517,7 @@ export default {
       mdTouchRipple: true,
       iosTouchRipple: false,
       touchRippleElements:
-        '.ripple, .link, .item-link, .list-button, .links-list a, .button, button, .input-clear-button, .dialog-button, .tab-link, .item-radio, .item-checkbox, .actions-button, .searchbar-disable-button, .fab a, .checkbox, .radio, .data-table .sortable-cell:not(.input-cell), .notification-close-button, .stepper-button, .stepper-button-minus, .stepper-button-plus, .menu-item-content, .list.accordion-list .accordion-item-toggle',
+        '.ripple, .link, .item-link, .list label.item-content, .list-button, .links-list a, .button, button, .input-clear-button, .dialog-button, .tab-link, .item-radio, .item-checkbox, .actions-button, .searchbar-disable-button, .fab a, .checkbox, .radio, .data-table .sortable-cell:not(.input-cell), .notification-close-button, .stepper-button, .stepper-button-minus, .stepper-button-plus, .menu-item-content, .list.accordion-list .accordion-item-toggle',
       touchRippleInsetElements:
         '.ripple-inset, .icon-only, .searchbar-disable-button, .input-clear-button, .notification-close-button, .md .navbar .link.back',
     },
